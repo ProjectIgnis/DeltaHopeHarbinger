0e6c9a4afaeb33b1e921fe089991e0634c2d9a34
NaimSantos <naimsantos45@gmail.com>
2020-04-05 18:50:32 +0000

    Removed deprecated flag

---
+++ script/official/c35129241.lua
@@ -0,0 +1,64 @@
+--星屑の願い
+--Stardust Wish
+--Scripted by Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--spsummon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EVENT_RELEASE)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1,0,EFFECT_COUNT_CODE_SINGLE)
+	e2:SetCondition(s.spcon)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+	--indestructable
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e3:SetTarget(s.intg)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+end
+s.listed_series={0xa3}
+function s.filter(c,e,tp,re)
+	local re=c:GetReasonEffect()
+	local rc=re:GetHandler()
+	return c:IsSetCard(0xa3) and c:IsType(TYPE_SYNCHRO) and c:IsPreviousControler(tp)
+		and c:IsReason(REASON_COST) and rc==c
+		and c:IsCanBeEffectTarget(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.filter,1,nil,e,tp,re)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return eg:IsContains(chkc) and s.filter(chkc,e,tp,re) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+	Duel.SetTargetCard(eg)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,eg,#eg,0,0)
+	Duel.SetChainLimit(s.chainlm)
+end
+function s.chainlm(e,rp,tp)
+	return tp==rp
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if e:GetHandler():IsRelateToEffect(e) and tc:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP) then
+		tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(id,2))
+	end
+end
+function s.intg(e,c)
+	return c:IsAttackPos() and c:GetFlagEffect(id)~=0
+end
