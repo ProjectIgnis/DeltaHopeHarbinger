61b75cf4b500a9505f54d66a7df1476512bef3e6
NaimSantos <naimsantos45@gmail.com>
2020-04-06 01:10:51 +0000

    Fix "Saber Slash"

---
+++ script/official/c11052544.lua
@@ -0,0 +1,38 @@
+--セイバー・スラッシュ
+--Saber Slash
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x100d}
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsAttackPos() and c:IsSetCard(0x100d)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then
+		local ct=Duel.GetMatchingGroupCount(s.cfilter,tp,LOCATION_MZONE,0,nil)
+		e:SetLabel(ct)
+		return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(aux.TRUE),tp,LOCATION_ONFIELD,LOCATION_ONFIELD,ct,c)
+	end
+	local ct=e:GetLabel()
+	local sg=Duel.GetMatchingGroup(aux.FilterFaceupFunction(aux.TRUE),tp,LOCATION_ONFIELD,LOCATION_ONFIELD,c)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,sg,ct,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local ct=Duel.GetMatchingGroupCount(s.cfilter,tp,LOCATION_MZONE,0,nil)
+	local g=Duel.GetMatchingGroup(aux.FilterFaceupFunction(aux.TRUE),tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+	if #g>=ct then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local sg=g:Select(tp,ct,ct,nil)
+		Duel.HintSelection(sg)
+		Duel.Destroy(sg,REASON_EFFECT)
+	end
+end
