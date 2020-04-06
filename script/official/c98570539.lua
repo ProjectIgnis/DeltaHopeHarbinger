33b7cc40fa9503e41b2db633c05b3c7e45337e8a
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-04-06 02:01:01 +0000

    "Dream Mirror of Chaos" fix
    Should now work properly if both "Dream Mirror of Joy" and "Dream Mirror of Terror" are up on the field.

---
+++ script/official/c98570539.lua
@@ -0,0 +1,33 @@
+--Dream Mirror of Chaos
+--Scripted by edo9300
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Fusion.CreateSummonEff({handler=c,fusfilter=aux.FilterBoolFunction(Card.IsSetCard,0x131),matfilter=Fusion.OnFieldMat,extrafil=s.fextra,extraop=s.extraop})
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	c:RegisterEffect(e1)
+	if not GhostBelleTable then GhostBelleTable={} end
+	table.insert(GhostBelleTable,e1)
+end
+s.listed_series={0x131}
+s.listed_names={CARD_DREAM_MIRROR_JOY,CARD_DREAM_MIRROR_TERROR}
+function s.fextra(e,tp,mg)
+	local joy=Duel.IsEnvironment(CARD_DREAM_MIRROR_JOY,PLAYER_ALL,LOCATION_FZONE)
+	local terror=Duel.IsEnvironment(CARD_DREAM_MIRROR_TERROR,PLAYER_ALL,LOCATION_FZONE) and not Duel.IsPlayerAffectedByEffect(tp,69832741)
+	local joyg=Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToGrave),tp,LOCATION_HAND,0,nil)
+	local terrorg=Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToRemove),tp,LOCATION_GRAVE,0,nil)
+	if joy and terror then
+		joyg:Merge(terrorg)
+		return joyg
+	end
+	if joy then
+		return joyg
+	end
+	if terror then
+		return terrorg
+	end
+end
+function s.extraop(e,tc,tp,sg)
+	local g1=sg:Filter(Card.IsLocation,nil,LOCATION_GRAVE)
+	Duel.Remove(g1,POS_FACEUP,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+	sg:Sub(g1)
+end
