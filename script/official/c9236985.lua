d5e04cc4bb6d0ebc08faf3fc228f7478b9458f4b
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-04-06 00:32:55 +0000

    "Gishki Photomirror" fix
    Missing "not".

---
+++ script/official/c9236985.lua
@@ -0,0 +1,46 @@
+--リチュアの写魂鏡
+--Gishki Photomirror
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	if not s.ritual_matching_function then
+		s.ritual_matching_function={}
+	end
+	s.ritual_matching_function[c]=aux.FilterEqualFunction(Card.IsSetCard,0x3a)
+end
+s.listed_series={0x3a}
+function s.filter(c,e,tp,lp)
+	if not c:IsRitualMonster() or not c:IsSetCard(0x3a) 
+		or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,true,false) then return false end
+	return lp>c:GetLevel()*500
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local lp=Duel.GetLP(tp)
+		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+			and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp,lp)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local lp=Duel.GetLP(tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local tg=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp,lp)
+	local tc=tg:GetFirst()
+	if tc then
+		mustpay=true
+		Duel.PayLPCost(tp,tc:GetLevel()*500)
+		mustpay=false
+		tc:SetMaterial(nil)
+		Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,true,false,POS_FACEUP)
+		tc:CompleteProcedure()
+	end
+end
