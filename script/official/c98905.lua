23719c7b741b0d5997fa1f8ca67ba5163e1c0418
senpaizuri3 <senpaizuri3@gmail.com>
2020-04-07 15:02:31 +0000

    Small fix "Cipher Spectrum"
    Now the effect Special Summons the target as well as the monster from the Extra Deck.

---
+++ script/official/c98905.lua
@@ -0,0 +1,65 @@
+--光波分光
+--Cipher Spectrum
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	aux.GlobalCheck(s,function()
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_LEAVE_FIELD_P)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+	end)
+end
+s.listed_series={0xe5}
+function s.cfilter(c)
+	return c:IsSetCard(0xe5) and c:IsType(TYPE_XYZ) and c:IsControler(c:GetOwner()) and c:GetOverlayCount()>0
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	if not eg then return end
+	local sg=eg:Filter(s.cfilter,nil)
+	local tc=sg:GetFirst()
+	for tc in aux.Next(sg) do
+		tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD&~(RESET_TOGRAVE|RESET_LEAVE),0,1)
+	end
+end
+function s.filter(c,e,tp,rp)
+	return c:GetFlagEffect(id)~=0 and c:IsLocation(LOCATION_GRAVE) and c:IsControler(tp)
+		and (c:IsReason(REASON_BATTLE) or (c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()~=tp))
+		and c:IsCanBeEffectTarget(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c:GetCode(),rp)
+end
+function s.spfilter(c,e,tp,cd,rp)
+	return c:IsType(TYPE_XYZ) and c:IsCode(cd) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)	and Duel.GetLocationCountFromEx(tp,rp,nil,c)>0
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return eg:IsContains(chkc) and s.filter(chkc,e,tp) end
+	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.GetUsableMZoneCount(tp)>1 and eg:IsExists(s.filter,1,nil,e,tp,rp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=eg:FilterSelect(tp,s.filter,1,1,nil,e,tp)
+	Duel.SetTargetCard(g)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 or Duel.GetUsableMZoneCount(tp)<=1 then return end
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc:GetCode())
+		if #g>0 then
+			local sg=Group.FromCards(tc)
+			sg:Merge(g)
+			Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
