da9386f45256cd4c302d4c8f4590cdfc440e59b5
ahtelel <32773013+ahtelel@users.noreply.github.com>
2020-04-07 02:50:02 +0000

    Fixed 'Aerial Recharge'
    Fixed count limit for token summoning effect

---
+++ script/official/c70875955.lua
@@ -0,0 +1,59 @@
+--空中補給
+--Aerial Recharge
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--spsummon
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetHintTiming(0,TIMING_END_PHASE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+	--cost
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetCountLimit(1)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE)
+	e3:SetOperation(s.mtop)
+	c:RegisterEffect(e3)
+end
+s.listed_series={0x101b}
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanSpecialSummonMonster(tp,31533705,0x101b,TYPES_TOKEN,0,0,3,RACE_MACHINE,ATTRIBUTE_WIND)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+	and Duel.IsPlayerCanSpecialSummonMonster(tp,31533705,0x101b,TYPES_TOKEN,0,0,3,RACE_MACHINE,ATTRIBUTE_WIND) then
+		local token=Duel.CreateToken(tp,id+1)
+		Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.rfilter(c)
+	return c:IsType(TYPE_TOKEN) or c:IsSetCard(0x101b)
+end
+function s.mtop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.CheckReleaseGroupCost(tp,s.rfilter,1,false,nil,nil)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		local g=Duel.SelectReleaseGroupCost(tp,s.rfilter,1,1,false,nil,nil)
+		Duel.Release(g,REASON_RULE)
+	else
+		Duel.SendtoGrave(e:GetHandler(),REASON_RULE)
+	end
+end
