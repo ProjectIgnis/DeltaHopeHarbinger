baebcb3f5d6fe39ebbcbbd6aed42c5bf819667a6
NaimSantos <naimsantos45@gmail.com>
2020-04-08 14:58:15 +0000

    Added missing function call to "Losr Guardian"

---
+++ script/official/c45871897.lua
@@ -0,0 +1,16 @@
+--ロストガーディアン
+--Lost Guardian
+local s,id=GetID()
+function s.initial_effect(c)
+	--base defense
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SET_BASE_DEFENSE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(s.defval)
+	c:RegisterEffect(e1)
+end
+function s.defval(e,c)
+	return Duel.GetMatchingGroupCount(aux.FilterFaceupFunction(Card.IsRace,RACE_ROCK),c:GetControler(),LOCATION_REMOVED,0,nil)*700
+end
