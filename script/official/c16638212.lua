ebb956048dd275d637e3d0e60f3d11c14a989256
ahtelel <32773013+ahtelel@users.noreply.github.com>
2020-04-10 03:00:38 +0000

    Fixed 'D.D. Sprite'
    Was not returning banished monster

---
+++ script/official/c16638212.lua
@@ -0,0 +1,46 @@
+--異次元の精霊
+--D.D. Sprite
+local s,id=GetID()
+function s.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local rg=Duel.GetMatchingGroup(Card.IsAbleToRemoveAsCost,tp,LOCATION_MZONE,0,nil):Filter(Card.IsFaceup,nil)
+	local g=aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),1,tp,HINTMSG_REMOVE,nil,nil,true)
+	if #g>0 then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+		return true
+	end
+	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.Remove(g,POS_FACEUP,REASON_COST+REASON_TEMPORARY)
+	g:GetFirst():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,0)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e1:SetCountLimit(1)
+	e1:SetReset((RESET_EVENT|RESETS_STANDARD_DISABLE)&~RESET_TOFIELD++RESET_PHASE+PHASE_STANDBY)
+	e1:SetOperation(s.retop)
+	e1:SetLabelObject(g:GetFirst())
+	c:RegisterEffect(e1)
+	g:DeleteGroup()
+end
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabelObject():GetFlagEffect(id)~=0 then
+		Duel.ReturnToField(e:GetLabelObject())
+	end
+end
