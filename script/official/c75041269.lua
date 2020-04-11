86381b96b99bd94d0685d2a2fbff22d34dff71db
NaimSantos <naimsantos45@gmail.com>
2020-04-11 21:26:07 +0000

    Fix "Clock Tower Prison"
    Fixed a bug where the player would be unable to Special Summo the listed monster even if the card had the ammount of counters required

---
+++ script/official/c75041269.lua
@@ -0,0 +1,86 @@
+--幽獄の時計塔
+--Clock Tower Prison
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableCounterPermit(0x1b)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--add counter
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_COUNTER)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetCountLimit(1)
+	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e2:SetCondition(s.ctcon)
+	e2:SetOperation(s.ctop)
+	c:RegisterEffect(e2)
+	--avoid battle damage
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTargetRange(1,0)
+	e3:SetCondition(s.dcon)
+	c:RegisterEffect(e3)
+	--register a flag to allow special summon
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e4:SetCode(EVENT_LEAVE_FIELD_P)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e4:SetOperation(s.regop)
+	c:RegisterEffect(e4)
+	--special summon
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,1))
+	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e5:SetCode(EVENT_TO_GRAVE)
+	e5:SetCondition(s.spcon)
+	e5:SetTarget(s.sptg)
+	e5:SetOperation(s.spop)
+	e5:SetLabelObject(e4)
+	c:RegisterEffect(e5)
+end
+s.counter_place_list={0x1b}
+s.listed_names={40591390}
+function s.dcon(e)
+	return e:GetHandler():GetCounter(0x1b)>=4
+end
+function s.ctcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp
+end
+function s.ctop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():AddCounter(0x1b,1)
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():GetCounter(0x1b)>=4 then
+		e:SetLabel(1)
+	else
+		e:SetLabel(0)
+	end
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_DESTROY) and e:GetLabelObject():GetLabel()==1
+end
+function s.spfilter(c,e,tp)
+	return c:IsCode(40591390) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
+	if g and #g:GetCount()>0 then
+		Duel.SpecialSummon(g,1,tp,tp,false,false,POS_FACEUP)
+	end
+end
