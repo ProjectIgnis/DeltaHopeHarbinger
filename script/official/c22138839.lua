9bd6ccf212b3f7d3c9100c4bc01960bec2488285
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-04-11 02:38:39 +0000

    "Predaplant Banksiogre" fix

---
+++ script/official/c22138839.lua
@@ -0,0 +1,70 @@
+--捕食植物バンクシアオーガ
+--Predaplant Banksiogre
+local s,id=GetID()
+function s.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.hspcon)
+	e1:SetTarget(s.hsptg)
+	e1:SetOperation(s.hspop)
+	c:RegisterEffect(e1)
+	--counter
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_COUNTER)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetCondition(s.ccon)
+	e2:SetOperation(s.cop)
+	c:RegisterEffect(e2)
+end
+s.counter_place_list={COUNTER_PREDATOR}
+function s.rfilter(c,tp)
+	return c:GetCounter(COUNTER_PREDATOR)>0 and c:IsControler(1-tp)
+end
+function s.hspcon(e,c)
+	if c==nil then return true end
+	local tp=e:GetHandlerPlayer()
+	return Duel.CheckReleaseGroup(tp,s.rfilter,1,false,1,true,c,tp,nil,true,nil,tp)
+end
+function s.hsptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectReleaseGroup(tp,s.rfilter,1,1,false,true,true,c,nil,nil,true,nil,tp)
+	if g then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+	return true
+	end
+	return false
+end
+function s.hspop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.Release(g,REASON_COST)
+	g:DeleteGroup()
+end
+function s.ccon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
+end
+function s.cop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	local tc=g:GetFirst()
+	for tc in aux.Next(g) do
+		tc:AddCounter(COUNTER_PREDATOR,1)
+		if tc:GetLevel()>1 then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_CHANGE_LEVEL)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			e1:SetCondition(s.lvcon)
+			e1:SetValue(1)
+			tc:RegisterEffect(e1)
+		end
+	end
+end
+function s.lvcon(e)
+	return e:GetHandler():GetCounter(COUNTER_PREDATOR)>0
+end
