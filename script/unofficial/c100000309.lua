5bb28056c6a53a20a76a3769511db2ce1beed076
NaimSantos <naimsantos45@gmail.com>
2020-04-17 18:06:38 +0000

    Fix "Wisel Attack 5"
    Previously, the script would look for the incorrect player when taking the damage battle to double

---
+++ script/unofficial/c100000309.lua
@@ -0,0 +1,106 @@
+--ワイゼルＡ５
+--Wisel Attack 5
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--selfdes
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_SELF_DESTROY)
+	e2:SetCondition(s.sdcon)
+	c:RegisterEffect(e2)
+	--direct attack
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_PIERCE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(LOCATION_MZONE,0)
+	e3:SetTarget(s.tg)
+	c:RegisterEffect(e3)
+	--negate
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(67098114,0))
+	e4:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_CHAINING)
+	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCondition(s.discon)
+	e4:SetTarget(s.distg)
+	e4:SetOperation(s.disop)
+	c:RegisterEffect(e4)
+	--double damage
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCode(EVENT_PRE_BATTLE_DAMAGE)
+	e5:SetCondition(s.dcon)
+	e5:SetOperation(s.dop)
+	c:RegisterEffect(e5)
+end
+s.listed_series={0x3013}
+s.listed_names={100000048}
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.CheckReleaseGroup(c:GetControler(),Card.IsCode,1,false,1,true,c,c:GetControler(),nil,false,nil,100000048)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectReleaseGroup(tp,Card.IsCode,1,1,false,true,true,c,nil,nil,false,nil,100000048)
+	if g then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+	return true
+	end
+	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.Release(g,REASON_COST)
+	g:DeleteGroup()
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x3013)
+end
+function s.sdcon(e,tp,eg,ep,ev,re,r,rp)
+	return not Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler())
+end
+function s.tg(e,c)
+	return c:IsFaceup() and c:IsSetCard(0x3013)
+end
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and Duel.GetAttacker() and Duel.GetAttacker():IsSetCard(0x3013) 
+		and ep~=tp and re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_TRAP) and Duel.IsChainNegatable(ev)
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateActivation(ev)
+	if re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+	end
+end
+function s.dcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:GetFirst():IsSetCard(0x3013)
+end
+function s.dop(e,tp,eg,ep,ev,re,r,rp)
+Debug.Message(Duel.GetBattleDamage(tp))
+	Duel.ChangeBattleDamage(1-tp,Duel.GetBattleDamage(1-tp)*2)
+end
