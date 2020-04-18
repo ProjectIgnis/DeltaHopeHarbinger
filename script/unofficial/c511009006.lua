6fd745cd66a90f859eb1a7a7df99edd0acfb426e
NaimSantos <naimsantos45@gmail.com>
2020-04-18 16:28:37 +0000

    Updated "Mystic Factor Melchior" to use the Fusion Procedure

---
+++ script/unofficial/c511009006.lua
@@ -0,0 +1,15 @@
+--Mystic Factor Melchior
+local s,id=GetID()
+function s.initial_effect(c)
+	--special summon
+	local params = {nil,Fusion.OnFieldMat,nil,nil,Fusion.ForcedHandler}
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(Fusion.SummonEffTG(table.unpack(params)))
+	e1:SetOperation(Fusion.SummonEffOP(table.unpack(params)))
+	c:RegisterEffect(e1)
+end
