29c62aa02c425b1a5efbb58cc8ca41f904601ea2
NaimSantos <naimsantos45@gmail.com>
2020-04-18 18:24:40 +0000

    Use the Fusion Summon procedure when it is possible
    This commit changes 3 anime cards to use the fusion summon procedure instead of manually performing the effect in the card script: Performapal Odd-Eyes Dissolver, Soprano the Melodious Songstress, Destiny HERO - Dusktopia

---
+++ script/unofficial/c511002485.lua
@@ -0,0 +1,15 @@
+--幻奏の歌姫ソプラノ
+--Soprano the Melodious Songstress (Anime)
+--rescripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--perform a fusion summon
+	local params = {nil,Fusion.CheckWithHandler(Fusion.OnFieldMat,nil,nil,Fusion.ForcedHandler}
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(Fusion.SummonEffTG(table.unpack(params)))
+	e1:SetOperation(Fusion.SummonEffOP(table.unpack(params)))
+	c:RegisterEffect(e1)
+end
---
+++ script/unofficial/c511009376.lua
@@ -0,0 +1,108 @@
+--Destiny HERO - Dusktopia Anime
+--fixed by MLD
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,90579153,aux.FilterBoolFunctionEx(Card.IsSetCard,0xc008))
+	--summon success
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(s.regcon)
+	e1:SetOperation(s.regop)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.fuscon)
+	e1:SetTarget(Fusion.SummonEffTG())
+	e1:SetOperation(Fusion.SummonEffOP())
+	c:RegisterEffect(e2)
+	--Protection
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetTarget(s.indtg)
+	e3:SetOperation(s.indop)
+	c:RegisterEffect(e3)
+	--
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_CHAINING)
+	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL+EFFECT_FLAG_CARD_TARGET)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1)
+	e4:SetCondition(s.prcon)
+	e4:SetTarget(s.prtg)
+	e4:SetOperation(s.prop)
+	c:RegisterEffect(e4)
+end
+s.material_setcode={0x8,0xc008}
+function s.regcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetSummonType()==SUMMON_TYPE_FUSION
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+end
+function s.fuscon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetFlagEffect(id)~=0
+end
+function s.indtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+end
+function s.indop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		local e3=e1:Clone()
+		e3:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+		tc:RegisterEffect(e3)
+	end
+end
+function s.filter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsOnField()
+end
+function s.prcon(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) then return false end
+	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DESTROY)
+	if ex and tg~=nil and tc+tg:FilterCount(s.filter,nil)-#tg>0 then
+		e:SetLabelObject(tg)
+		return true end
+	return false
+end
+function s.prtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local g=e:GetLabelObject()
+	if chkc then return g:IsContains(chkc) end
+	if chk==0 then return g and eg:IsExists(Card.IsCanBeEffectTarget,1,nil,e) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local sg=g:FilterSelect(tp,Card.IsCanBeEffectTarget,1,1,nil,e)
+	Duel.SetTargetCard(sg)
+end
+function s.prop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+		e1:SetValue(1)
+		e1:SetReset(RESET_CHAIN)
+		tc:RegisterEffect(e1)
+	end
+end
