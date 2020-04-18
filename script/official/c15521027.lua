22defa18967de1ae71f841d030ecc8a008fcafba
EerieCode <eeriecode@gmail.com>
2020-04-18 08:49:37 +0000

    Merge pull request #179 from ProjectIgnis/upgrade-dice
    Updated cards with dice rolling effects

---
+++ script/official/c15521027.lua
@@ -0,0 +1,111 @@
+--D・スマホン
+--Morphtronic Smartfon
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.cona)
+	e2:SetTarget(s.tga)
+	e2:SetOperation(s.opa)
+	c:RegisterEffect(e2)
+	--sort deck
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetCondition(s.cond)
+	e3:SetTarget(s.tgd)
+	e3:SetOperation(s.opd)
+	c:RegisterEffect(e3)
+end
+s.roll_dice=true
+s.listed_series={0x26}
+function s.spfilter(c,tp)
+	return c:IsSetCard(0x26) and c:IsType(TYPE_MONSTER) and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true)
+		and (Duel.GetLocationCount(tp,LOCATION_MZONE)>0 or (c:IsLocation(LOCATION_MZONE) and c:GetSequence()<5))
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_GRAVE,0,nil)
+	return aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),0)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_GRAVE,0,nil)
+	local g=aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),1,tp,HINTMSG_REMOVE,nil,nil,true)
+	if #g>0 then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+		return true
+	end
+	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+	g:DeleteGroup()
+end
+function s.cona(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsAttackPos()
+end
+function s.tga(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>0 end
+	Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
+end
+function s.filter(c)
+	return c:IsSetCard(0x26) and c:IsAbleToHand()
+end
+function s.opa(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)==0 then return end
+	local dc=Duel.TossDice(tp,1)
+	Duel.ConfirmDecktop(tp,dc)
+	local dg=Duel.GetDecktopGroup(tp,dc)
+	local g=dg:Filter(s.filter,nil)
+	if #g>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
+	Duel.ShuffleDeck(tp)
+end
+function s.cond(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsDefensePos()
+end
+function s.tgd(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>0 end
+	Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
+end
+function s.opd(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)==0 then return end
+	local dc=Duel.TossDice(tp,1)
+	local g=Duel.GetDecktopGroup(tp,dc)
+	local ct=#g
+	Duel.ConfirmCards(tp,g)
+	local op=Duel.SelectOption(tp,aux.Stringid(id,3),aux.Stringid(id,4))
+	Duel.SortDecktop(tp,tp,ct)
+	if op==0 then return end
+	for i=1,ct do
+		local tg=Duel.GetDecktopGroup(tp,1)
+		Duel.MoveSequence(tg:GetFirst(),1)
+	end
+end
