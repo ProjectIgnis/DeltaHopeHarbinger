a77486022f8eaaa7869a659584d69c469d6fe4cc
NaimSantos <naimsantos45@gmail.com>
2020-04-19 16:10:11 +0000

    Rescripted "D/D Swirl Slime (anime)" and "D/D Necro Slime (anime)"
    They should now use the Fusion Summon Procedure

---
+++ script/unofficial/c511001405.lua
@@ -0,0 +1,14 @@
+--D/D Swirl Slime (anime)
+--rescripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion summon
+	local params = {nil,Fusion.InHandMat(Card.IsSetCard,0xaf),nil,nil,Fusion.ForcedHandler}
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetTarget(Fusion.SummonEffTG(table.unpack(params)))
+	e1:SetOperation(Fusion.SummonEffOP(table.unpack(params)))
+	c:RegisterEffect(e1)
+end
---
+++ script/unofficial/c511001454.lua
@@ -0,0 +1,15 @@
+--D/D Necro Slime (anime)
+--rescripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--spsummon
+	local params = {aux.FilterBoolFunction(Card.IsSetCard,0x10af),Fusion.OnFieldMat}
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_GRAVE)
+	e1:SetCost(aux.bfgcost)
+	e1:SetTarget(Fusion.SummonEffTG(table.unpack(params)))
+	e1:SetOperation(Fusion.SummonEffOP(table.unpack(params)))
+	c:RegisterEffect(e1)
+end
