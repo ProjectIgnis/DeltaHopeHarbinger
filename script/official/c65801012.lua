37f8722355cb40c644a8666ccc346a69df0fd4e3
NaimSantos <naimsantos45@gmail.com>
2020-04-21 16:48:38 +0000

    Fix "Cynet Fusion"
    Fixed an incorrect function name that would cause it to be unable to use a Link monster from the Graveyard even if the conditions were met

---
+++ script/official/c65801012.lua
@@ -0,0 +1,31 @@
+--サイバネット・フュージョン
+--Cynet Fusion
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Fusion.CreateSummonEff(c,aux.FilterBoolFunction(Card.IsRace,RACE_CYBERSE),nil,s.fextra,s.extraop)
+	c:RegisterEffect(e1)
+	if not GhostBelleTable then GhostBelleTable={} end
+	table.insert(GhostBelleTable,e1)
+end
+function s.fcheck(tp,sg,fc)
+	return sg:FilterCount(Card.IsLocation,nil,LOCATION_GRAVE)<=1
+end
+function s.fextra(e,tp,mg)
+	if not Duel.IsExistingMatchingCard(Card.IsInExtraMZone,tp,LOCATION_MZONE,0,1,nil,tp) and not Duel.IsPlayerAffectedByEffect(tp,69832741) then
+		local eg=Duel.GetMatchingGroup(s.exfilter0,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil)
+		if #eg>0 then
+			return eg,s.fcheck
+		end
+	end
+	return nil
+end
+function s.exfilter0(c)
+	return c:IsLinkMonster() and c:IsRace(RACE_CYBERSE) and c:IsAbleToRemove()
+end
+function s.extraop(e,tc,tp,sg)
+	local rg=sg:Filter(Card.IsLocation,nil,LOCATION_GRAVE)
+	if #rg>0 then
+		Duel.Remove(rg,POS_FACEUP,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+		sg:Sub(rg)
+	end
+end
