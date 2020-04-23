a4f5278edb5a6fed3f4ddbf6e924328111fe3f53
NaimSantos <naimsantos45@gmail.com>
2020-04-23 11:14:04 +0000

    Make "Kingyo Sukui"'s effect once per turn

---
+++ script/official/c9251497.lua
@@ -0,0 +1,49 @@
+--金魚救い
+--Kingyo Sukui
+--Scripted by Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--excavate
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_TODECK+CATEGORY_TOHAND+CATEGORY_TOGRAVE+CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(s.tdtg)
+	e2:SetOperation(s.tdop)
+	c:RegisterEffect(e2)
+end
+function s.tdfilter(c)
+	return c:IsFaceup() and c:IsAbleToDeck()
+end
+function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and s.tdfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.tdfilter,tp,0,LOCATION_MZONE,1,nil) and Duel.IsPlayerCanDiscardDeck(tp,1) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectTarget(tp,s.tdfilter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
+end
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if not c:IsRelateToEffect(e) or not tc:IsRelateToEffect(e) or tc:IsFacedown() or not Duel.IsPlayerCanDiscardDeck(tp,1) then return end
+	Duel.ConfirmDecktop(tp,1)
+	local dc=Duel.GetDecktopGroup(tp,1):GetFirst()
+	if not dc then return end
+	if dc:IsType(TYPE_MONSTER) and dc:IsAttribute(tc:GetAttribute()) then
+		if Duel.SendtoHand(dc,nil,REASON_EFFECT)~=0 then
+			Duel.SendtoDeck(tc,nil,2,REASON_EFFECT)
+		end
+	else
+		if Duel.SendtoGrave(dc,REASON_EFFECT) then
+			Duel.Destroy(c,REASON_EFFECT)
+		end
+	end
+end
