20251e559bfb7dda167c64935a06db6586e4e168
NaimSantos <naimsantos45@gmail.com>
2020-04-23 11:36:36 +0000

    Fix "Miracle Contact"'s interaction with "Necrovalley"

---
+++ script/official/c35255456.lua
@@ -0,0 +1,15 @@
+--ミラクル・コンタクト
+--Miracle Contact
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	c:RegisterEffect(Fusion.CreateSummonEff(c,s.spfilter,Card.IsAbleToDeck,s.fextra,Fusion.ShuffleMaterial,nil,nil,nil,0,nil,FUSPROC_NOTFUSION))
+end
+s.listed_series={0x3008}
+s.listed_names={CARD_NEOS}
+function s.spfilter(c)
+	return c:IsSetCard(0x3008) and aux.IsMaterialListCode(c,CARD_NEOS)
+end
+function s.fextra(e,tp,mg)
+	return Duel.GetMatchingGroup(aux.NecroValleyFilter(Fusion.IsMonsterFilter(Card.IsAbleToDeck)),tp,LOCATION_GRAVE,0,nil)
+end
