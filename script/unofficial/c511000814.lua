2aaae5e68eecb25789a445e3e5fce6522baa20fd
NaimSantos <naimsantos45@gmail.com>
2020-04-25 21:11:52 +0000

    Rescript "Extra Fusion" to use the Fusion Summon procedure

---
+++ script/unofficial/c511000814.lua
@@ -0,0 +1,22 @@
+--Extra Fusion
+--rescripted by Naim (to match the Fusion summon procedure)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Fusion.CreateSummonEff(c,nil,aux.FALSE,s.fextra)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	c:RegisterEffect(e1)
+end
+function s.fcheck(tp,sg,fc)
+	return sg:FilterCount(Card.IsLocation,nil,LOCATION_EXTRA)<=2
+end
+function s.fextra(e,tp,mg)
+	local sg=Duel.GetMatchingGroup(s.exfilter,tp,LOCATION_EXTRA,0,nil)
+	if #sg>0 then
+		return sg,s.fcheck
+	end
+	return nil
+end
+function s.exfilter(c)
+	return c:IsType(TYPE_FUSION) and c:IsAbleToGrave()
+end
