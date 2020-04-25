6d007c5e18601f54d508e7a6f514bbca8b764114
NaimSantos <naimsantos45@gmail.com>
2020-04-25 16:47:57 +0000

    Fix "Night Dragolich"

---
+++ script/official/c88724332.lua
@@ -0,0 +1,29 @@
+--ナイト・ドラゴリッチ
+--Night Dragolich
+local s,id=GetID()
+function s.initial_effect(c)
+	--position change
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SET_POSITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(s.target)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetValue(POS_FACEUP_DEFENSE)
+	c:RegisterEffect(e1)
+	--defense decrease
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetTarget(s.target)
+	e2:SetValue(s.defval)
+	c:RegisterEffect(e2)
+end
+function s.target(e,c)
+	return c:IsFaceup() and not c:IsRace(RACE_WYRM) and c:IsSummonType(SUMMON_TYPE_SPECIAL) and (c:GetSummonLocation()&LOCATION_DECK+LOCATION_EXTRA)~=0
+end
+function s.defval(e,c)
+	return -c:GetBaseDefense()
+end
