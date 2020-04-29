ee13e0f6f77f0e8458a6c4c57302382393ac1b0a
NaimSantos <naimsantos45@gmail.com>
2020-04-29 12:11:57 +0000

    Rescript "Beastborg Fusioner"

---
+++ script/unofficial/c511001548.lua
@@ -0,0 +1,33 @@
+--Beastborg Fusioner
+--rescripted by Naim (to match the Fusion Summon Procedure)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--perform a Fusion Summon
+	local params = {aux.FilterBoolFunction(Card.IsSetCard,0x50b)}
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(Fusion.SummonEffTG(table.unpack(params)))
+	e2:SetOperation(Fusion.SummonEffOP(table.unpack(params)))
+	c:RegisterEffect(e2)
+	--cannot normal summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(1,0)
+	e3:SetCode(EFFECT_CANNOT_SUMMON)
+	c:RegisterEffect(e3)
+	--cannot set
+	local e4=e3:Clone()
+	e4:SetCode(EFFECT_CANNOT_MSET)
+	c:RegisterEffect(e4)
+end
