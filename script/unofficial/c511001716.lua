e98da1ed6debe67066b3dd8a8939434ed02aacb7
NaimSantos <naimsantos45@gmail.com>
2020-04-29 12:00:51 +0000

    Rescript "Frontline Fusion"

---
+++ script/unofficial/c511001716.lua
@@ -0,0 +1,11 @@
+--Frontline Fusion
+--rescripted by Naim (to match the Fusion Summon Procedure)
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Fusion.CreateSummonEff(c)
+	e1:SetCondition(s.condition)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp and Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE
+end
