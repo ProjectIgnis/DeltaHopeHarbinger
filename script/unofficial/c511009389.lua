480352ea4471976f9bb11a270a5d82b8e9c51edd
NaimSantos <naimsantos45@gmail.com>
2020-04-29 12:19:26 +0000

    Rescript "Dowsing Fusion (anime)"

---
+++ script/unofficial/c511009389.lua
@@ -0,0 +1,16 @@
+--Dowsing Fusion (anime)
+--rescripted by Naim (to match the Fusion Summon Procedure)
+function s.initial_effect(c)
+	--Activate
+	local e1=Fusion.CreateSummonEff(c,nil,aux.FALSE,s.fextra,s.extraop)
+	c:RegisterEffect(e1)
+	if not GhostBelleTable then GhostBelleTable={} end
+	table.insert(GhostBelleTable,e1)
+end
+function s.fextra(e,tp,mg)
+	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToExtra),tp,LOCATION_GRAVE,0,nil):Filter(Card.IsType,nil,TYPE_PENDULUM)
+end
+function s.extraop(e,tc,tp,sg)
+	Duel.SendtoExtraP(sg,nil,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+	sg:Clear()
+end
