a0a8d62d702b78384251e576ac2368fb91751fa9
NaimSantos <naimsantos45@gmail.com>
2020-04-30 19:18:54 +0000

    Fix "Traptrix Trap Hole Nightmare"
    Check for the status of the triggering card instead of raising a global effect for flag registration

---
+++ script/official/c29616929.lua
@@ -0,0 +1,34 @@
+--蟲惑の落とし穴
+--Traptrix Trap Hole Nightmare
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DISABLE+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local loc=Duel.GetChainInfo(0,CHAININFO_TRIGGERING_LOCATION)
+	return re:GetHandler():IsControler(1-tp) and re:GetHandler():IsStatus(STATUS_SPSUMMON_TURN)
+		and loc==LOCATION_MZONE and Duel.IsChainDisablable(ev)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=re:GetHandler()
+	if not tc:IsDisabled() then
+		if Duel.NegateEffect(ev) and tc:IsRelateToEffect(re) then
+			Duel.Destroy(eg,REASON_EFFECT)
+		end
+	end
+end
