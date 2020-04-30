f31a915f5f7c728fc83e253a9fb39ee24c87eb0d
NaimSantos <naimsantos45@gmail.com>
2020-04-30 00:05:55 +0000

    Rescript "Speed Spell - Miracle Fusion"

---
+++ script/unofficial/c100100007.lua
@@ -0,0 +1,24 @@
+--Ｓｐ－ミラクル·フュージョ
+--Speed Spell - Miracle Fusion
+--rescripted by Naim to match the Fusion Summon procedure
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Fusion.CreateSummonEff(c,aux.FilterBoolFunction(Card.IsSetCard,0x3008),aux.FALSE,s.fextra,Fusion.BanishMaterial)
+	e1:SetCost(s.cost)
+	c:RegisterEffect(e1)
+	if not GhostBelleTable then GhostBelleTable={} end
+	table.insert(GhostBelleTable,e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=Duel.GetFieldCard(tp,LOCATION_SZONE,5)
+	if chk==0 then return tc and tc:IsCanRemoveCounter(tp,0x91,3,REASON_COST) end	 
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+	tc:RemoveCounter(tp,0x91,3,REASON_COST)	
+end
+function s.fextra(e,tp,mg)
+	if not Duel.IsPlayerAffectedByEffect(tp,69832741) then
+		return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToRemove),tp,LOCATION_GRAVE,0,nil)
+	end
+	return nil
+end
