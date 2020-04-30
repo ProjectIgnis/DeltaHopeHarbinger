55c11ce24c9e6275ba998a2e0e554b105b83cdc8
larry126 <28985622+larry126@users.noreply.github.com>
2020-04-30 12:37:43 +0000

    Cleaned up Plus/Minus handling and others
    Also removed Benkai Guard due to the lack of Benkai Token DEF

---
+++ script/unofficial/c170000112.lua
@@ -0,0 +1,7 @@
+--磁石の戦士 Ω－
+--Magnet Warrior Ω－
+Duel.LoadScript("c419.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	c:AddPlusMinusAttribute()
+end
---
+++ script/unofficial/c170000113.lua
@@ -0,0 +1,7 @@
+--磁石の戦士 Σ－
+--Magnet Warrior Σ－
+Duel.LoadScript("c419.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	c:AddPlusMinusAttribute()
+end
