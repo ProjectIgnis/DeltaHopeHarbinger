3775fb6bd0040e0c6f416634fc90ec15e90b5eb8
NaimSantos <naimsantos45@gmail.com>
2020-05-04 12:03:42 +0000

    Added missing reset value to "Blackwing - Mistral the Silver Shield"

---
+++ script/official/c46710683.lua
@@ -0,0 +1,24 @@
+--BF－銀盾のミストラル
+--Blackwing - Mistral the Silver Shield
+local s,id=GetID()
+function s.initial_effect(c)
+	--change damage
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetCondition(s.condition)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD) and e:GetHandler():IsReason(REASON_DESTROY)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL+PHASE_END)
+end
