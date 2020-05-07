70f458e6c3fa4ad2c6a00835bc48133e2133ea3c
NaimSantos <naimsantos45@gmail.com>
2020-05-07 11:43:21 +0000

    Fix "Cynet Codec"
    Fixed a bug where the player would be unable to select which "Cynet Codec" to trigger if multiples were face-up on the field

---
+++ script/official/c60018643.lua
@@ -0,0 +1,111 @@
+--サイバネット・コーデック
+--Cynet Codec
+--Scripted by Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--search
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_CUSTOM+id)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+	local g=Group.CreateGroup()
+	g:KeepAlive()
+	e2:SetLabelObject(g)
+	--mass register
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetLabelObject(e2)
+	e3:SetOperation(s.regop)
+	c:RegisterEffect(e3)
+	--register attributes
+	aux.GlobalCheck(s,function()
+		s.attr_list={}
+		s.attr_list[0]=0
+		s.attr_list[1]=0
+		aux.AddValuesReset(function()
+				s.attr_list[0]=0
+				s.attr_list[1]=0
+			end)
+		end)
+end
+s.listed_series={0x101}
+function s.thcfilter(c,e,tp)
+	local attr=c:GetAttribute()
+	return c:IsSetCard(0x101) and c:IsControler(tp)
+		and c:IsLocation(LOCATION_MZONE) and c:IsPreviousLocation(LOCATION_EXTRA)
+		and s.attr_list[tp]&attr==0
+		and c:IsCanBeEffectTarget(e)
+		and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,attr)
+end
+function s.thfilter(c,attr)
+	return c:IsRace(RACE_CYBERSE) and c:IsAttribute(attr) and c:IsAbleToHand()
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	local tg=eg:Filter(s.thcfilter,nil,e,tp)
+	if #tg>0 then
+		local g=e:GetLabelObject():GetLabelObject()
+		if Duel.GetCurrentChain()==0 then g:Clear() end
+		g:Merge(tg)
+		e:GetLabelObject():SetLabelObject(g)
+		Duel.RaiseSingleEvent(e:GetHandler(),EVENT_CUSTOM+id,e,0,tp,tp,0)
+	end
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local g=e:GetLabelObject():Filter(s.thcfilter,nil,e,tp)
+	if chkc then return g:IsContains(chkc) and s.thcfilter(chkc,e,tp) end
+	if chk==0 then return #g>0 and Duel.GetFlagEffect(tp,id)==0 end
+	Duel.RegisterFlagEffect(tp,id,RESET_CHAIN,0,1)
+	if #g==1 then
+		Duel.SetTargetCard(g:GetFirst())
+	else
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+		local tc=g:Select(tp,1,1,nil)
+		Duel.SetTargetCard(tc)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	local att=tc:GetAttribute()
+	if not c:IsRelateToEffect(e) then return end
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,att)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+			s.attr_list[tp]=s.attr_list[tp]|att
+		end
+		for _,str in aux.GetAttributeStrings(att) do
+			c:RegisterFlagEffect(0,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,EFFECT_FLAG_CLIENT_HINT,1,0,str)
+		end
+	end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(s.splimit)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	aux.RegisterClientHint(e:GetHandler(),nil,tp,1,0,aux.Stringid(id,2),nil)
+end
+function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return not c:IsRace(RACE_CYBERSE) and c:IsLocation(LOCATION_EXTRA)
+end
