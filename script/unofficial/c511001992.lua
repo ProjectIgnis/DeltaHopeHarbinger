d60593181bfe9b1042c314943f87e44855d4a6a3
larry126 <28985622+larry126@users.noreply.github.com>
2020-05-11 02:07:55 +0000

    Fix Hell Tuning and Beelze
    Hell Tuning should only removes "Hell" monsters as cost
    Beelze only gains ATK for the damage its controller took

---
+++ script/unofficial/c511001992.lua
@@ -0,0 +1,41 @@
+--魔王龍 ベエルゼ (Manga)
+--Beelze of the Diabolic Dragons (Manga)
+local s,id,alias=GetID()
+function s.initial_effect(c)
+	alias=c:Alias()
+	--synchro summon
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99)
+	c:EnableReviveLimit()
+	--indes
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--atk
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(alias,0))
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_DAMAGE)
+	e3:SetCondition(s.atkcon)
+	e3:SetOperation(s.atkop)
+	c:RegisterEffect(e3)
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return ep==tp
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(ev)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+		c:RegisterEffect(e1)
+	end
+end
