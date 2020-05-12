89b7bf5ae1515cf094cb833bafee5f9f5f18f9b4
NaimSantos <naimsantos45@gmail.com>
2020-05-12 11:40:00 +0000

    Fix "Eternal Reverse"

---
+++ script/unofficial/c100000364.lua
@@ -0,0 +1,45 @@
+--Eternal Reverse
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddEquipProcedure(c)
+	--change position
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.tstg)
+	e1:SetOperation(s.tsop)
+	c:RegisterEffect(e1)
+	--destruction replacement
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_EQUIP)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetTarget(s.destg)
+	e2:SetOperation(s.desop)
+	c:RegisterEffect(e2)
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsCanTurnSet()
+end
+function s.tstg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_SZONE) and s.filter(chkc) and c:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_SZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_SZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
+end
+function s.tsop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		Duel.ChangePosition(tc,POS_FACEDOWN)
+	end
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local tg=c:GetEquipTarget()
+	if chk==0 then return c and not c:IsStatus(STATUS_DESTROY_CONFIRMED) and tg and tg:IsReason(REASON_BATTLE) end
+	return Duel.SelectYesNo(tp,aux.Stringid(id,0))
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Destroy(e:GetHandler(),REASON_EFFECT+REASON_REPLACE)
+end
