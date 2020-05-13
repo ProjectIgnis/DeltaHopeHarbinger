2957253c8860dab53145c50cc9eb99672121f195
larry126 <28985622+larry126@users.noreply.github.com>
2020-05-13 18:17:28 +0000

    Updated effects that access detach as cost
    To adapt the new implementation of gained effects

---
+++ script/unofficial/c511002868.lua
@@ -0,0 +1,83 @@
+--ＲＲ－ラピッド・エクシーズ
+--Raidraptor - Rapid Xyz
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_BATTLE_START+TIMING_BATTLE_END)
+	e1:SetCondition(s.xyzcon)
+	e1:SetTarget(s.xyztg)
+	e1:SetOperation(s.xyzop)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0xba}
+function s.cfilter(c)
+	return c:IsSummonType(SUMMON_TYPE_SPECIAL)
+end
+function s.xyzcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE  
+		and Duel.IsExistingMatchingCard(s.cfilter,tp,0,LOCATION_MZONE,1,nil)
+end
+function s.xyzfilter(c)
+	return c:IsXyzSummonable() and c:IsSetCard(0xba)
+end
+function s.xyztg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_EXTRA,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.xyzop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local tc=Duel.SelectMatchingCard(tp,s.xyzfilter,tp,LOCATION_EXTRA,0,1,1,nil):GetFirst()
+	if tc then
+		Duel.XyzSummon(tp,tc)
+		if not tc:IsHasEffect(511002571) then return end
+		local effs={tc:GetCardEffect(511002571)}
+		local te=nil
+		local acd={}
+		local ac={}
+		for _,teh in ipairs(effs) do
+			local temp=teh:GetLabelObject()
+			if temp:GetType()&EFFECT_TYPE_GRANT==EFFECT_TYPE_GRANT then temp=temp:GetLabelObject() end
+			local con=temp:GetCondition()
+			local cost=temp:GetCost()
+			local tg=temp:GetTarget()
+			if (not con or con(temp,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE))
+				and (not cost or cost(temp,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE,0))
+				and (not tg or tg(temp,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE,0)) then
+				table.insert(ac,teh)
+				table.insert(acd,temp:GetDescription())
+			end
+		end
+		if #ac<=0 or not Duel.SelectEffectYesNo(tp,tc) then return end
+		Duel.BreakEffect()
+		if #ac==1 then te=ac[1] elseif #ac>1 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EFFECT)
+			local op=Duel.SelectOption(tp,table.unpack(acd))+1
+			te=ac[op]
+		end
+		if not te then return end
+		local teh=te
+		te=teh:GetLabelObject()
+		if te:GetType()&EFFECT_TYPE_GRANT==EFFECT_TYPE_GRANT then te=te:GetLabelObject() end
+		local cost=te:GetCost()
+		if cost then cost(te,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE,1) end
+		local tg=te:GetTarget()
+		if tg then tg(te,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE,1) end
+		Duel.BreakEffect()
+		tc:CreateEffectRelation(te)
+		Duel.BreakEffect()
+		local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+		for etc in aux.Next(g) do
+			etc:CreateEffectRelation(te)
+		end
+		local operation=te:GetOperation()
+		if operation then operation(te,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE,1) end
+		tc:ReleaseEffectRelation(te)
+		for etc in aux.Next(g) do
+			etc:ReleaseEffectRelation(te)
+		end
+	end
+end
---
+++ script/unofficial/c511003038.lua
@@ -0,0 +1,129 @@
+--アルティメット・フルバースト
+--Ultimate Full Burst
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE 
+		and re and re:GetHandler():IsSetCard(0x95)
+end
+function s.cfilter(c,tp)
+	return c:IsFaceup() and c:IsSummonPlayer(tp) and c:IsType(TYPE_XYZ) and c:IsSummonType(SUMMON_TYPE_XYZ)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local g=eg:Filter(s.cfilter,nil,tp)
+	local tc=g:GetFirst()
+	if chkc then return g:IsContains(chkc) end
+	if chk==0 then
+		if not tc or not tc:IsHasEffect(511002571) or #g~=1 or not tc:IsCanBeEffectTarget(e) then return false end
+		local eff={tc:GetCardEffect(511002571)}
+		for _,teh in ipairs(eff) do
+			local temp=teh:GetLabelObject()
+			if temp:GetType()&EFFECT_TYPE_GRANT==EFFECT_TYPE_GRANT then temp=temp:GetLabelObject() end
+			local con=temp:GetCondition()
+			local cost=temp:GetCost()
+			local tg=temp:GetTarget()
+			if (not con or con(temp,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE))
+				and (not cost or cost(temp,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE,0))
+				and (not tg or tg(temp,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE,0)) then
+				return true
+			end
+		end
+		return false
+	end
+	Duel.SetTargetCard(g)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		local eff={tc:GetCardEffect(511002571)}
+		local te=nil
+		local acd={}
+		local ac={}
+		for _,teh in ipairs(eff) do
+			local temp=teh:GetLabelObject()
+			if temp:GetType()&EFFECT_TYPE_GRANT==EFFECT_TYPE_GRANT then temp=temp:GetLabelObject() end
+			local con=temp:GetCondition()
+			local cost=temp:GetCost()
+			local tg=temp:GetTarget()
+			if (not con or con(temp,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE))
+				and (not cost or cost(temp,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE,0))
+				and (not tg or tg(temp,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE,0)) then
+				table.insert(ac,teh)
+				table.insert(acd,temp:GetDescription())
+			end
+		end
+		if #ac==1 then te=ac[1] elseif #ac>1 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EFFECT)
+			op=Duel.SelectOption(tp,table.unpack(acd))
+			op=op+1
+			te=ac[op]
+		end
+		if not te then return end
+		local teh=te
+		te=teh:GetLabelObject()
+		if te:GetType()&EFFECT_TYPE_GRANT==EFFECT_TYPE_GRANT then te=te:GetLabelObject() end
+		local cost=te:GetCost()
+		if cost then cost(te,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE,1) end
+		local tg=te:GetTarget()
+		if tg then tg(te,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE,1) end
+		Duel.BreakEffect()
+		tc:CreateEffectRelation(te)
+		Duel.BreakEffect()
+		local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+		for etc in aux.Next(g) do
+			etc:CreateEffectRelation(te)
+		end
+		local operation=te:GetOperation()
+		if operation then operation(te,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE,1) end
+		tc:ReleaseEffectRelation(te)
+		for etc in aux.Next(g) do
+			etc:ReleaseEffectRelation(te)
+		end
+		Duel.BreakEffect()
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_MUST_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		local e3=Effect.CreateEffect(e:GetHandler())
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_ATTACK_ALL)
+		e3:SetValue(1)
+		e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e3)
+		local fid=e:GetHandler():GetFieldID()
+		tc:RegisterFlagEffect(51103038,RESET_EVENT+RESETS_STANDARD,0,1,fid)
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e2:SetCode(EVENT_PHASE+PHASE_END)
+		e2:SetCountLimit(1)
+		e2:SetLabel(fid)
+		e2:SetLabelObject(tc)
+		e2:SetCondition(s.descon)
+		e2:SetOperation(s.desop)
+		Duel.RegisterEffect(e2,tp)
+	end
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	if tc:GetFlagEffectLabel(51103038)==e:GetLabel() then
+		return true
+	else
+		e:Reset()
+		return false
+	end
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	Duel.Destroy(tc,REASON_EFFECT)
+end
