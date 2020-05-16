9e487ce2599b6c762c67d0ddb1705e17463e5fde
NaimSantos <naimsantos45@gmail.com>
2020-05-16 19:11:03 +0000

    Fix "Re-Qliate"
    Rescripted the card to work both when activating it after being face-down and using the effect after it is already face-up on the field

---
+++ script/official/c20426907.lua
@@ -0,0 +1,113 @@
+--機殻の再星
+--Re-qliate
+local s,id=GetID()
+function s.initial_effect(c)
+	Duel.EnableGlobalFlag(GLOBALFLAG_SELF_TOGRAVE)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--negate
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DISABLE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTarget(s.distg)
+	e2:SetOperation(s.disop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_DISABLE)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetTarget(s.distg2)
+	e4:SetOperation(s.disop2)
+	c:RegisterEffect(e4)
+	--send itself to the Grave
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetCode(EFFECT_SELF_TOGRAVE)
+	e5:SetCondition(s.sdcon)
+	c:RegisterEffect(e5)
+end
+s.listed_names={id}
+s.listed_series={0xaa}
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		if e:GetHandler():IsStatus(STATUS_EFFECT_ENABLED) then
+			return eg:GetFirst():IsLevelBelow(4)
+		else
+			return not eg:GetFirst():IsType(TYPE_NORMAL) and eg:GetFirst():IsLevelBelow(4)
+		end
+	end
+	Duel.SetTargetCard(eg)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		Duel.NegateRelatedChain(tc,RESET_TURN_SET)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetValue(RESET_TURN_SET)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e2)
+	end
+end
+function s.ngtfilter(c,check)
+	return c:IsFaceup() and c:IsLevelAbove(5) and (not c:IsType(TYPE_NORMAL) or check)
+end
+function s.distg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	local check=e:GetHandler():IsStatus(STATUS_EFFECT_ENABLED)
+	if chk==0 then return eg:IsExists(s.ngtfilter,1,nil,check) end
+	local gp=eg:Filter(s.ngtfilter,nil,check)
+	Duel.SetTargetCard(gp)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,gp,#gp,0,0)
+end
+function s.disop2(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local gp=Duel.GetTargetCards(e):Filter(Card.IsFaceup,nil)
+	local tc=gp:GetFirst()
+	for tc in aux.Next(gp) do
+		Duel.NegateRelatedChain(tc,RESET_TURN_SET)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetValue(RESET_TURN_SET)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e2)
+		local e3=Effect.CreateEffect(e:GetHandler())
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e3:SetReset(RESET_EVENT+RESETS_REDIRECT)
+		e3:SetValue(LOCATION_REMOVED)
+		tc:RegisterEffect(e3)
+	end
+end
+function s.sdfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0xaa) and not c:IsCode(id)
+end
+function s.sdcon(e)
+	return not Duel.IsExistingMatchingCard(s.sdfilter,e:GetHandlerPlayer(),LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+end
