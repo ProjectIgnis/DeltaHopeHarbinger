469a15f01975d9e5c77eee551badd658d1bf5789
larry126 <28985622+larry126@users.noreply.github.com>
2020-05-16 13:40:16 +0000

    Unofficial Additional Normal Summon fix

---
+++ script/unofficial/c511000374.lua
@@ -0,0 +1,34 @@
+--魔の海域レベル３
+--Cursed Waters Level 3
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_names={CARD_UMI}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsEnvironment(CARD_UMI) and Duel.GetFlagEffect(tp,id)==0
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsPlayerCanSummon(tp) and Duel.IsPlayerCanAdditionalSummon(tp) end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+	e1:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
+	e1:SetTarget(aux.TargetBoolFunction(s.sumfilter))
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+end
+function s.sumfilter(c)
+	return c:GetLevel()<=3
+end
