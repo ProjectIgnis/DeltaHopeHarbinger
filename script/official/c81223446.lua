67c06afaea95f9be4353f57f4941f83f8d67ce8a
NaimSantos <naimsantos45@gmail.com>
2020-05-18 19:05:10 +0000

    Fix "Necro Fusion"
    Fixed a bug where the monster would still be able to attack during the turn it was Fusion Summoned

---
+++ script/official/c81223446.lua
@@ -0,0 +1,32 @@
+--死魂融合
+--Necro Fusion
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Fusion.CreateSummonEff(c,nil,s.matfilter,s.fextra,s.extraop,nil,s.stage2)
+	c:RegisterEffect(e1)
+	if not GhostBelleTable then GhostBelleTable={} end
+	table.insert(GhostBelleTable,e1)
+end
+function s.matfilter(c)
+	return aux.SpElimFilter(c) and c:IsAbleToRemove(tp,POS_FACEDOWN)
+end
+function s.fextra(e,tp,mg)
+	if not Duel.IsPlayerAffectedByEffect(tp,69832741) then
+		return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToRemove),tp,LOCATION_GRAVE,0,nil)
+	end
+	return nil
+end
+function s.extraop(e,tc,tp,sg)
+	Duel.Remove(sg,POS_FACEDOWN,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+	sg:Clear()
+end
+function s.stage2(e,tc,tp,sg,chk)
+	if chk==1 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1,true)
+	end
+end
