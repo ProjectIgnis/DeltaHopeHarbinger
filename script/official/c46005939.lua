8d1fddfb62342a22741f9b877c5f5d92c65c6bcf
edo9300 <edoardo762@gmail.com>
2020-05-19 11:09:56 +0000

    Fix pendulum xyz (legal and manga)

---
+++ script/official/c46005939.lua
@@ -0,0 +1,82 @@
+--ペンデュラム・エクシーズ
+--Pendulum Xyz
+--Scripted by Eerie Code, based on anime version by Larry126
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.filter(c,e,tp)
+	return c:IsFaceup() and c:GetLevel()>0 and c:IsCanBeEffectTarget(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+local function reglevel(c,tc,lv)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_XYZ_LEVEL)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	e1:SetValue(lv)
+	tc:RegisterEffect(e1,true)
+	return e1
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_PZONE,0,nil,e,tp)
+	if chk==0 then
+		if not (#g==2 and Duel.IsPlayerCanSpecialSummonCount(tp,2)
+			and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+			and Duel.GetLocationCount(tp,LOCATION_MZONE)>1) then return false end
+		local tc1=g:GetFirst()
+		local tc2=g:GetNext()
+		local e1=reglevel(e:GetHandler(),tc1,tc2:GetLevel())
+		local e2=reglevel(e:GetHandler(),tc2,tc1:GetLevel())
+		local res=Duel.IsExistingMatchingCard(Card.IsXyzSummonable,tp,LOCATION_EXTRA,0,1,nil,g,g)
+		if e1 then e1:Reset() end
+		if e2 then e2:Reset() end
+		return res
+	end
+	Duel.SetTargetCard(g)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,2,0,0)
+end
+function s.filter2(c,e,tp)
+	return c:IsRelateToEffect(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<2 then return end
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(s.filter2,nil,e,tp)
+	if #g<2 then return end
+	for tc in aux.Next(g) do
+		Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		tc:RegisterEffect(e2)
+	end
+	Duel.SpecialSummonComplete()
+	Duel.BreakEffect()
+	local tc1=g:GetFirst()
+	local tc2=g:GetNext()
+	local e1=reglevel(e:GetHandler(),tc1,tc2:GetLevel())
+	local e2=reglevel(e:GetHandler(),tc2,tc1:GetLevel())
+	local xyzg=Duel.GetMatchingGroup(Card.IsXyzSummonable,tp,LOCATION_EXTRA,0,nil,g,g)
+	if xyzg and #xyzg>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local xyz=xyzg:Select(tp,1,1,nil):GetFirst()
+		Duel.XyzSummon(tp,xyz,g,g)
+	else
+		if e1 then e1:Reset() end
+		if e2 then e2:Reset() end
+	end
+end
---
+++ script/unofficial/c511003000.lua
@@ -0,0 +1,55 @@
+--ペンデュラム・エクシーズ
+--Pendulum XYZ
+--fixed by Larry126
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+local function reglevel(c,tc,lv)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_XYZ_LEVEL)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	e1:SetValue(lv)
+	tc:RegisterEffect(e1,true)
+	return e1
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local g=Duel.GetFieldGroup(tp,LOCATION_PZONE,0)
+		if #g~=2 then return false end
+		local tc1=g:GetFirst()
+		local tc2=g:GetNext()
+		local e1=reglevel(e:GetHandler(),tc1,tc2:GetLevel())
+		local e2=reglevel(e:GetHandler(),tc2,tc1:GetLevel())
+		local res=Duel.IsExistingMatchingCard(Card.IsXyzSummonable,tp,LOCATION_EXTRA,0,1,nil,g,g)
+		if e1 then e1:Reset() end
+		if e2 then e2:Reset() end
+		return res
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local pg=Duel.GetFieldGroup(tp,LOCATION_PZONE,0)
+	if #pg~=2 then return false end
+	local tc1=pg:GetFirst()
+	local tc2=pg:GetNext()
+	local e1=reglevel(e:GetHandler(),tc1,tc2:GetLevel())
+	local e2=reglevel(e:GetHandler(),tc2,tc1:GetLevel())
+	local g=Duel.GetMatchingGroup(Card.IsXyzSummonable,tp,LOCATION_EXTRA,0,nil,pg,pg)
+	if g and #g>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local xyz=g:Select(tp,1,1,nil):GetFirst()
+		Duel.XyzSummon(tp,xyz,pg,pg)
+	else
+		if e1 then e1:Reset() end
+		if e2 then e2:Reset() end
+	end
+end
