f4d34ad7307618e238a2a267e5409351c6d1489f
larry126 <28985622+larry126@users.noreply.github.com>
2020-05-19 12:11:25 +0000

    Updated all unoffiicial cards that activate from Deck

---
+++ script/unofficial/c100000331.lua
@@ -0,0 +1,50 @@
+--Ｆａｉｒｙ Ｔａｌｅ 第二章 暴怒の太陽
+--Fairy Tale Chapter 2: Seething Sun
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--unnegatable
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_PREDRAW)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetCountLimit(1,0,EFFECT_COUNT_CODE_SINGLE)
+	e3:SetCondition(s.accon)
+	e3:SetOperation(s.acop)
+	c:RegisterEffect(e3)
+	local e2=e3:Clone()
+	e2:SetCountLimit(1,0,EFFECT_COUNT_CODE_SINGLE)
+	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	c:RegisterEffect(e2)
+	--damage
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetCode(EFFECT_CHANGE_DAMAGE)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e4:SetTargetRange(1,1)
+	e4:SetValue(s.value)
+	c:RegisterEffect(e4)
+end
+function s.value(e,re,dam,r,rp,rc)
+	if (r&REASON_BATTLE)~=0 then
+		return dam*2
+	else
+		return dam
+	end
+end
+function s.accon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp and e:GetHandler():GetTurnID()~=Duel.GetTurnCount()
+end
+function s.filter(c,tp)
+	return c:IsCode(100000332) and c:GetActivateEffect():IsActivatable(tp,true,true)
+end
+function s.acop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE+LOCATION_HAND+LOCATION_DECK,0,1,1,nil,tp):GetFirst()
+	aux.PlayFieldSpell(tc,e,tp,eg,ep,ev,re,r,rp)
+end
---
+++ script/unofficial/c100000333.lua
@@ -0,0 +1,73 @@
+--Ｆａｉｒｙ Ｔａｌｅ最終章 忘却の妖月
+--Fairy Tale Final Chapter: Moon of Oblivion
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
+	e2:SetCode(EVENT_CHANGE_POS)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTarget(s.postg)
+	e2:SetOperation(s.posop)
+	c:RegisterEffect(e2)	
+	--unnegatable
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_PREDRAW)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetCountLimit(1,0,EFFECT_COUNT_CODE_SINGLE)
+	e3:SetCondition(s.actcond)
+	e3:SetOperation(s.actop)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCountLimit(1,0,EFFECT_COUNT_CODE_SINGLE)
+	e4:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	c:RegisterEffect(e4)
+end
+s.listed_names={100000330}
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	if (r&REASON_RULE)~=0 and Duel.GetTurnCount()==1 then
+		--obsolete
+		Duel.RegisterFlagEffect(tp,62765383,0,0,1)
+		Duel.RegisterFlagEffect(1-tp,62765383,0,0,1)
+	end
+end
+function s.posfilter(c,e)
+	return (c:GetPreviousPosition()&POS_DEFENSE)~=0 and c:IsPosition(POS_FACEUP_ATTACK) 
+		and (not e or c:IsRelateToEffect(e))
+end
+function s.postg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return eg:IsExists(s.posfilter,1,nil) and rp==tp end
+	Duel.SetTargetCard(eg)
+end
+function s.posop(e,tp,eg,ep,ev,re,r,rp)
+	local g=eg:Filter(s.posfilter,nil,e)
+	local tc=g:GetFirst()
+	while tc do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(tc:GetAttack()*2)
+		tc:RegisterEffect(e1)
+		tc=g:GetNext()
+	end
+end
+function s.actcond(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp and e:GetHandler():GetTurnID()~=Duel.GetTurnCount()
+end
+function s.filter(c,tp)
+	return c:IsCode(100000330) and c:GetActivateEffect():IsActivatable(tp,true,true)
+end
+function s.actop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE+LOCATION_HAND+LOCATION_DECK,0,1,1,nil,tp):GetFirst()
+	aux.PlayFieldSpell(tc,e,tp,eg,ep,ev,re,r,rp)
+end
+
