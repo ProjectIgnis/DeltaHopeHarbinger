01312317867e76dc7633f58c5c28034cfc76abde
larry126 <28985622+larry126@users.noreply.github.com>
2020-05-22 03:19:25 +0000

    Fix 5ds anime cards
    Sneak Giant's NS proc fixed
    Star Excursion overhaul

---
+++ script/unofficial/c511000873.lua
@@ -0,0 +1,115 @@
+--Star Excursion
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetCondition(s.rmcon)
+	e1:SetTarget(s.rmtg)
+	e1:SetOperation(s.rmop)
+	c:RegisterEffect(e1)
+end
+function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local at=Duel.GetAttackTarget()
+	return at and a:IsType(TYPE_SYNCHRO) and at:IsType(TYPE_SYNCHRO)
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local a=Duel.GetAttacker()
+	local at=Duel.GetAttackTarget()
+	if chk==0 then return at and a:IsAbleToRemove() and at:IsAbleToRemove() end
+	local g=Group.FromCards(a,at)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,2,0,0)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local at=Duel.GetAttackTarget()
+	local g=Group.FromCards(a,at)
+	if Duel.Remove(g,0,REASON_EFFECT+REASON_TEMPORARY)~=0 and e:IsHasType(EFFECT_TYPE_ACTIVATE) then
+		local og=Duel.GetOperatedGroup()
+		for oc in aux.Next(og) do
+			if oc:IsControler(tp) then
+				oc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE+RESET_OPPO_TURN,0,4)
+			else
+				oc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE+RESET_SELF_TURN,0,4)
+			end
+		end
+		og:KeepAlive()
+		local c=e:GetHandler()
+		local res=Duel.GetTurnPlayer()~=tp and 4 or 3
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
+		e1:SetCountLimit(1)
+		e1:SetLabel(0)
+		e1:SetLabelObject(og)
+		e1:SetCondition(s.tccon)
+		e1:SetOperation(s.tcop)
+		e1:SetReset(RESET_PHASE+PHASE_BATTLE+RESET_OPPO_TURN,res)
+		Duel.RegisterEffect(e1,tp)
+		local e2=Effect.CreateEffect(c)
+		e2:SetDescription(aux.Stringid(id,0))
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE)
+		e2:SetCode(1082946)
+		e2:SetLabelObject(e1)
+		e2:SetOwnerPlayer(tp)
+		e2:SetOperation(s.reset)
+		e2:SetReset(RESET_PHASE+PHASE_BATTLE+RESET_OPPO_TURN,res)
+		c:RegisterEffect(e2)
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e3:SetCode(EVENT_PHASE+PHASE_BATTLE)
+		e3:SetCountLimit(1)
+		e3:SetLabelObject(e1)
+		e3:SetCondition(s.retcon)
+		e3:SetOperation(s.retop)
+		e3:SetReset(RESET_PHASE+PHASE_BATTLE+RESET_OPPO_TURN,res)
+		Duel.RegisterEffect(e3,tp)
+		Duel.RegisterFlagEffect(0,e1:GetFieldID()+id,RESET_PHASE+PHASE_END,0,1)
+	end
+end
+function s.reset(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetLabelObject() then e:Reset() return end
+	s.tcop(e:GetLabelObject(),tp,eg,ep,ev,e,r,rp)
+end
+function s.retfilter(c)
+	return c:GetFlagEffect(id)~=0
+end
+function s.tccon(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject()
+	if not g:IsExists(s.retfilter,1,nil) or e:GetLabel()==2 and Duel.GetTurnPlayer()~=tp then
+		g:DeleteGroup()
+		e:Reset()
+		return false
+	else return Duel.GetTurnPlayer()~=tp and Duel.GetFlagEffect(0,e:GetFieldID()+id)==0 end
+end
+function s.tcop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=e:GetLabel()+1
+	e:SetLabel(ct)
+	e:GetOwner():SetTurnCounter(ct)
+end
+function s.retcon(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetLabelObject() then e:Reset() return false end
+	local g=e:GetLabelObject():GetLabelObject()
+	if not g:IsExists(s.retfilter,1,nil) then
+		g:DeleteGroup()
+		e:Reset()
+		return false
+	else return Duel.GetTurnPlayer()~=tp and e:GetLabelObject():GetLabel()==2 end
+end
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetLabelObject() then e:Reset() return false end
+	local g=e:GetLabelObject():GetLabelObject()
+	local sg=g:Filter(s.retfilter,nil)
+	for tc in aux.Next(sg) do
+		Duel.ReturnToField(tc)
+		tc:SetStatus(STATUS_SUMMON_TURN+STATUS_FORM_CHANGED,false)
+	end
+	if re then re:Reset() end
+	g:DeleteGroup()
+	e:Reset()
+	e:GetLabelObject():Reset()
+end
---
+++ script/unofficial/c810000009.lua
@@ -0,0 +1,34 @@
+--スニーク・ジャイアント
+--Sneak Giant
+--Scripted by: UnknownGuest, updated by ClaireStanfield
+local s,id=GetID()
+function s.initial_effect(c)
+	-- summon with no tribute
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SUMMON_PROC)
+	e1:SetCondition(s.ntcon)
+	e1:SetOperation(s.ntop)
+	c:RegisterEffect(e1)
+end
+function s.filter(c,e,tp)
+	return c:IsLevel(1) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.ntcon(e,c,minc)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return minc==0 and c:GetLevel()>4 and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0
+		and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+end
+function s.ntop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
