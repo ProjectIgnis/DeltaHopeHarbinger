6f9510538b7df440cfa7c7f91d2a839575c5a23d
larry126 <28985622+larry126@users.noreply.github.com>
2020-05-23 11:20:46 +0000

    Fix Granel Top 5
    It should now correctly using "Infinity" archetype

---
+++ script/unofficial/c511008026.lua
@@ -0,0 +1,58 @@
+--グランエルＴ５
+--Granel Top 5
+--Scripted by Snrk
+Duel.LoadScript("c420.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--self destruction
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_SELF_DESTROY)
+	e1:SetCondition(s.sdcon)
+	c:RegisterEffect(e1)
+	--equip
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_EQUIP)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.eqcon)
+	e2:SetOperation(s.eqop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x562}
+--self destruction
+s.cfilter=aux.FilterFaceupFunction(Card.IsInfinity)
+function s.sdcon(e,tp,eg,ep,ev,re,r,rp)
+	return not Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+--equip
+function s.eqfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_SYNCHRO) and c:IsAbleToChangeControler()
+end
+function s.eqcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.eqfilter,tp,0,LOCATION_MZONE,1,nil)
+	and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+	and Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+end
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SELF)
+	local eqc=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	local eqtg=Duel.SelectMatchingCard(tp,s.eqfilter,tp,0,LOCATION_MZONE,1,1,nil):GetFirst()
+	if not eqg or not eqtgc then return end
+	if Duel.Equip(tp,eqtg,eqc,false) then
+		--add equip limit
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_EQUIP_LIMIT)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetValue(true)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		eqtg:RegisterEffect(e1)
+	else
+		Duel.SendtoGrave(eqtg,REASON_EFFECT)
+	end
+end
