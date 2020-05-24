d6f48acfe09c67eba05d33ebba27820e7a01de0a
larry126 <28985622+larry126@users.noreply.github.com>
2020-05-24 04:29:09 +0000

    Update unofficial Poly to use new fusion proc

---
+++ script/unofficial/c100000390.lua
@@ -0,0 +1,15 @@
+--超機合体 (Anime)
+--Overload Fusion (Anime)
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Fusion.CreateSummonEff(c,aux.FilterBoolFunction(Card.IsRace,RACE_MACHINE),Fusion.OnFieldMat(Card.IsAbleToRemove),s.fextra,Fusion.BanishMaterial)
+	c:RegisterEffect(e1)
+	if not GhostBelleTable then GhostBelleTable={} end
+	table.insert(GhostBelleTable,e1)
+end
+function s.fextra(e,tp,mg)
+	if not Duel.IsPlayerAffectedByEffect(tp,69832741) then
+		return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToRemove),tp,LOCATION_GRAVE,0,nil)
+	end
+	return nil
+end
---
+++ script/unofficial/c100100035.lua
@@ -0,0 +1,25 @@
+--Ｓｐ－オーバーロード·フュージョン
+--Speed Spell - Overload Fusion
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Fusion.CreateSummonEff(c,s.ffilter,Fusion.OnFieldMat(Card.IsAbleToRemove),s.fextra,Fusion.BanishMaterial)
+	e1:SetCost(s.cost)
+	c:RegisterEffect(e1)
+	if not GhostBelleTable then GhostBelleTable={} end
+	table.insert(GhostBelleTable,e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=Duel.GetFieldCard(tp,LOCATION_SZONE,5)
+	if chk==0 then return tc and tc:IsCanRemoveCounter(tp,0x91,3,REASON_COST) end	
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+	tc:RemoveCounter(tp,0x91,3,REASON_COST) 
+end
+function s.ffilter(c)
+	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_MACHINE)
+end
+function s.fextra(e,tp,mg)
+	if not Duel.IsPlayerAffectedByEffect(tp,69832741) then
+		return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToRemove),tp,LOCATION_GRAVE,0,nil)
+	end
+	return nil
+end
