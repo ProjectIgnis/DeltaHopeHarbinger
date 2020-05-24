359ff41adb2e7ba9f62d6518186452df2c3091eb
NaimSantos <naimsantos45@gmail.com>
2020-05-24 09:52:49 +0000

    Updated scripts for 27 skill cards

---
+++ script/skill/c300101004.lua
@@ -0,0 +1,50 @@
+--Royal Flush
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)
+end
+function s.cfilter(c,code, code2)
+	return (c:IsCode(code) or c:IsCode(code2)) and c:GetAttackAnnouncedCount()>0
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--"cost" check
+	if not (Duel.CheckLPCost(tp,1000) and not Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil,64788463,90876561)) then return false end
+	--condition
+	return aux.CanActivateSkill(tp)
+	and Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsCode,25652259),tp,LOCATION_ONFIELD,0,1,nil)
+	and Duel.IsPlayerCanAdditionalSummon(tp)
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--opt register
+	Duel.RegisterFlagEffect(ep,id,RESET_PHASE+PHASE_END,0,0)
+	--cost
+	Duel.PayLPCost(tp,1000)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_OATH+EFFECT_FLAG_IGNORE_IMMUNE)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsCode,64788463))
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+	e2:SetProperty(EFFECT_FLAG_OATH+EFFECT_FLAG_IGNORE_IMMUNE)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsCode,90876561))
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e2,tp)
+	
+	--additional normal summon
+	local e3=Effect.CreateEffect(e:GetHandler())
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+	e3:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
+	e3:SetTarget(aux.TargetBoolFunction(Card.IsCode,64788463))
+	e3:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e3,tp)
+end
