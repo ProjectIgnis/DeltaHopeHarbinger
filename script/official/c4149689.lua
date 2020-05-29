3c24613d003ec6b7cd785d90804215adca4f88d9
NaimSantos <naimsantos45@gmail.com>
2020-05-29 14:02:32 +0000

    Rescript "Goblin Fan"
    The effect should be continuously applied and not trigger-like

---
+++ script/official/c4149689.lua
@@ -0,0 +1,38 @@
+--天狗のうちわ
+--Goblin Fan
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(1,1)
+	e2:SetValue(s.aclimit)
+	c:RegisterEffect(e2)
+	--destroy
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetOperation(s.desop)
+	c:RegisterEffect(e3)
+end
+function s.aclimit(e,re,tp)
+	local rc=re:GetHandler()
+	return re:IsActiveType(TYPE_MONSTER) and re:GetActivateLocation()==LOCATION_MZONE and rc:GetFlagEffect(id)~=0
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	if eg then
+		local tc=eg:GetFirst()
+		if tc:IsLevelBelow(2) then
+			Duel.Destroy(tc,REASON_EFFECT)
+			tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
+		end
+	end
+end
