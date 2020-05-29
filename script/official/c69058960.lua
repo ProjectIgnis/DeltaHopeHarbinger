533c761674d6d746de3819897544bc67561e5279
NaimSantos <naimsantos45@gmail.com>
2020-05-29 14:19:58 +0000

    Updated "Number" monsters to use EFFECT_FLAG_PLAYER_TARGET
    Number 54: Lion Heart
    Number 13: Embodiment of Crime
    Number 31: Embodiment of Punishment

---
+++ script/official/c69058960.lua
@@ -0,0 +1,82 @@
+--No.13 ケインズ・デビル
+--Number 13: Embodiment of Crime
+local s,id=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,nil,1,2)
+	c:EnableReviveLimit()
+	--change battle position
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_POSITION)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
+	--cannot be destroyed
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e2:SetCondition(s.indcon)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	c:RegisterEffect(e3)
+	--reflect battle damage
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_REFLECT_BATTLE_DAMAGE)
+	e4:SetCondition(s.refcon)
+	e4:SetValue(1)
+	c:RegisterEffect(e4)
+end
+s.xyz_number=13
+s.listed_names={95442074}
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0 end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_MZONE)
+	if #g>0 then
+		Duel.ChangePosition(g,POS_FACEUP_ATTACK)
+		local fid=c:GetRealFieldID()
+		for tc in aux.Next(g) do
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_MUST_ATTACK)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffect(e1)
+			local e2=e1:Clone()
+			e2:SetCode(EFFECT_MUST_ATTACK_MONSTER)
+			e2:SetValue(s.atklimit)
+			e2:SetLabel(fid)
+			tc:RegisterEffect(e2)
+		end
+	end
+end
+function s.atklimit(e,c)
+	return c:GetRealFieldID()==e:GetLabel()
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsCode(95442074)
+end
+function s.indcon(e)
+	return Duel.IsExistingMatchingCard(s.filter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+		and e:GetHandler():GetOverlayCount()~=0
+end
+function s.refcon(e)
+	return Duel.IsExistingMatchingCard(s.filter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+		and Duel.GetAttackTarget()==e:GetHandler()
+end
