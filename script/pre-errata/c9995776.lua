5f0a8c8e93c012c3ee23554a89277f12d05c9256
NaimSantos <naimsantos45@gmail.com>
2020-06-01 16:29:46 +0000

    Renamed scripts

--- script/pre-errata/c9995776.lua
+++ script/pre-errata/c9995776.lua
@@ -1,5 +1,5 @@
 --宮廷のしきたり
---Imperial Custom, OCG
+--Imperial Custom (pre-errata), TCG
 local s,id=GetID()
 function s.initial_effect(c)
 	c:SetUniqueOnField(1,0,id)
@@ -8,19 +8,16 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--Continuous traps cannot be destroyed by battle or card effects
+	--Continuous traps cannot be destroyed
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_INDESTRUCTABLE)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetTargetRange(LOCATION_ONFIELD,LOCATION_ONFIELD)
 	e2:SetTarget(s.infilter)
-	e2:SetValue(s.indesval)
+	e2:SetValue(1)
 	c:RegisterEffect(e2)
 end
 function s.infilter(e,c)
 	return (c:GetType()&0x20004)==0x20004 and c:GetCode()~=id
-end
-function s.indesval(e,re,r,rp)
-	return (r&REASON_EFFECT+REASON_BATTLE)~=0
 end
