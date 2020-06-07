c832c7d94b046ad0cab1e59660a6609c86cdeff9
larry126 <28985622+larry126@users.noreply.github.com>
2020-06-07 09:48:27 +0000

    Updated Dark Sanctuary
    To reflect its anime usage and text better

---
+++ script/unofficial/c511004006.lua
@@ -0,0 +1,122 @@
+--ダーク・ネクロフィア (Manga)
+--Dark Necrofear (Manga)
+--Scripted by Edo9300
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--possess
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetCondition(s.con)
+	e2:SetOperation(s.op)
+	c:RegisterEffect(e2)
+	--
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_PHASE_START+PHASE_BATTLE)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetOperation(s.op2)
+	c:RegisterEffect(e3)
+	--Gain and Damage
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_DAMAGE+CATEGORY_RECOVER)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e4:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e4:SetRange(LOCATION_GRAVE)
+	e4:SetCondition(s.dmcon)
+	e4:SetTarget(s.dmtg)
+	e4:SetOperation(s.dmop)
+	c:RegisterEffect(e4)
+	aux.GlobalCheck(s,function()
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_DESTROYED)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+	end)
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	while tc do
+		if tc:GetFlagEffect(id)==0 and tc:IsType(TYPE_MONSTER) and (tc:GetPreviousPosition()==POS_FACEUP_ATTACK or tc:GetPreviousPosition()==POS_ATTACK) then
+			tc:RegisterFlagEffect(id+1,RESET_PHASE+PHASE_END,0,5)
+		end
+		tc=eg:GetNext()
+	end
+end
+function s.spfilter(c,tp)
+	return c:GetFlagEffect(id+1)>0 and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true)
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil)
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>-3 and #rg>2 and aux.SelectUnselectGroup(rg,e,tp,3,3,aux.ChkfMMZ(1),0)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil)
+	local g=aux.SelectUnselectGroup(rg,e,tp,3,3,aux.ChkfMMZ(1),1,tp,HINTMSG_REMOVE)
+	if #g>0 then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+		return true
+	end
+	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+	g:DeleteGroup()
+end
+function s.con(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	return e:GetHandler():GetLocation()==LOCATION_GRAVE
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	c:RegisterFlagEffect(id+2,0,0,1)
+	if Duel.GetCurrentPhase()>=8 and Duel.GetCurrentPhase()<=20 and Duel.GetTurnPlayer()~=tp then
+		local tc=Duel.SelectMatchingCard(c:GetOwner(),nil,c:GetOwner(),0,LOCATION_MZONE,1,1,nil):GetFirst()
+		if tc then
+			tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+		end
+	end
+end
+function s.op2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:GetFlagEffect(id+2)>0 and Duel.GetTurnPlayer()~=tp then
+		local tc=Duel.SelectMatchingCard(c:GetOwner(),nil,c:GetOwner(),0,LOCATION_MZONE,1,1,nil):GetFirst()
+		if tc then
+			tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+		end
+	end
+end
+function s.dmcon(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	return Duel.GetAttacker():GetFlagEffect(id)>0 and Duel.GetAttacker():GetControler()~=tp
+end
+function s.dmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return true end
+	local c=Duel.GetAttacker()
+	local dam=c:GetAttack()/2
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,dam)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,dam)
+end
+function s.dmop(e,tp,eg,ep,ev,re,r,rp)
+	local c=Duel.GetAttacker()
+	local dam=c:GetAttack()/2
+	if Duel.NegateAttack() then
+		Duel.Recover(tp,dam,REASON_EFFECT)
+		Duel.Damage(1-tp,dam,REASON_EFFECT)
+	end
+end
