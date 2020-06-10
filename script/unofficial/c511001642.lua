915194775bbad2389a3c23fecf0500bb11c78059
larry126 <28985622+larry126@users.noreply.github.com>
2020-06-10 15:20:24 +0000

    Merge branch 'master' of https://github.com/ProjectIgnis/CardScripts

---
+++ script/unofficial/c511001642.lua
@@ -0,0 +1,41 @@
+--シンクロ・アライランス
+--Synchro Alliance
+local s,id=GetID()
+function s.initial_effect(c)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsType(TYPE_SYNCHRO)
+		and Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsType,TYPE_SYNCHRO),0,LOCATION_MZONE,LOCATION_MZONE,1,c)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetFirstTarget()
+	if tg and tg:IsRelateToEffect(e) then
+		local code=tg:GetOriginalCode()
+		local g=Duel.GetMatchingGroup(aux.FilterFaceupFunction(Card.IsType,TYPE_SYNCHRO),tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+		local c=e:GetHandler()
+		for tc in aux.Next(g) do
+			if tc~=tg then tc:CopyEffect(code,RESET_EVENT+RESETS_STANDARD,1) end
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			e1:SetValue(Duel.GetMatchingGroupCount(Card.IsType,tc:GetControler(),LOCATION_GRAVE,0,nil,TYPE_SYNCHRO)*600)
+			tc:RegisterEffect(e1)
+		end
+	end
+end
