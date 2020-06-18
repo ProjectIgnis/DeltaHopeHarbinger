747ed10a5b6e83639a5abdb05ee89c2b4c1424d8
edo9300 <edoardo762@gmail.com>
2020-06-18 15:37:28 +0000

    Fix undefined values in Life Absorbing Machine

---
+++ script/official/c14318794.lua
@@ -0,0 +1,68 @@
+--生命吸収装置
+--Life Absorbing Machine
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(TIMING_STANDBY_PHASE,0)
+	c:RegisterEffect(e1)
+	--recover
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_RECOVER)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e2:SetCondition(s.reccon)
+	e2:SetTarget(s.rectg)
+	e2:SetOperation(s.recop)
+	c:RegisterEffect(e2)
+	aux.GlobalCheck(s,function()
+		s[0]={}
+		s[1]={}
+		s[2]={}
+		s[3]={}
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_PAY_LPCOST)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+		aux.AddValuesReset(function()
+			local p=Duel.GetTurnPlayer()
+			s[p+2]={table.unpack(s[p])}
+			s[p]={}
+		end)
+	end)
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	if ep==Duel.GetTurnPlayer() then
+		local val=math.ceil(ev/2)
+		table.insert(s[ep],val)
+	end
+end
+function s.reccon(e,tp,eg,ep,ev,re,r,rp)
+	return tp==Duel.GetTurnPlayer()
+end
+function s.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return #s[tp+2]>0 and (c:GetFlagEffect(id)==0 or s[tp+2][c:GetFlagEffectLabel(id)+1]) end
+	local rec
+	if c:GetFlagEffect(id)==0 then
+		rec=s[tp+2][1]
+		c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_STANDBY,0,1,1)
+	else
+		rec=s[tp+2][c:GetFlagEffectLabel(id)+1]
+		c:SetFlagEffectLabel(id,c:GetFlagEffectLabel(id)+1)
+	end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(rec)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,s[2][1])
+end
+function s.recop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Recover(p,d,REASON_EFFECT)
+end
