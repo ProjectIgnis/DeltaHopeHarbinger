3b589f0ccff08b46e0cbf19a0226c5890d048305
NaimSantos <naimsantos45@gmail.com>
2020-06-23 14:59:34 +0000

    Rescript "Performage String Figure" and  "Vision Fusion (Manga)"

---
+++ script/unofficial/c100000501.lua
@@ -0,0 +1,13 @@
+--幻影融合
+--Vision Fusion (Manga)
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Fusion.CreateSummonEff(c,aux.FilterBoolFunction(Card.IsSetCard,0x5008),aux.FALSE,s.fextra,nil,nil,nil,2)
+	c:RegisterEffect(e1)
+end
+function s.mfilter(c)
+	return c:IsAbleToGrave() and c:IsFaceup() and c:IsSetCard(0x5008)
+end
+function s.fextra(e,tp,mg)
+	return Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_SZONE,0,nil)
+end
---
+++ script/unofficial/c511009106.lua
@@ -0,0 +1,58 @@
+--Performage String Figure
+--fixed by MLD
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon itself
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(34475451,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	--Perform a Fusion Summon
+	local params = {nil,Fusion.OnFieldMat,nil,nil,Fusion.ForcedHandler}
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTarget(Fusion.SummonEffTG(table.unpack(params)))
+	e2:SetOperation(Fusion.SummonEffOP(table.unpack(params)))
+	c:RegisterEffect(e2)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetAttacker():GetControler()~=tp and Duel.GetAttackTarget()==nil
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		local a=Duel.GetAttacker()
+		if a:CanAttack() and not a:IsImmuneToEffect(e) then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+			e1:SetValue(1)
+			e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
+			c:RegisterEffect(e1)
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e2:SetCode(EVENT_PRE_BATTLE_DAMAGE)
+			e2:SetOperation(s.damop)
+			e2:SetReset(RESET_PHASE+PHASE_DAMAGE)
+			Duel.RegisterEffect(e2,tp)
+			Duel.CalculateDamage(a,c)
+		end
+	end
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.ChangeBattleDamage(tp,0)
+end
