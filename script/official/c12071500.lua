c384447215d24e45cb88e19566d301a7f65424ba
Naim <naimsantos45@gmail.com>
2020-06-24 16:15:27 +0000

    Fix "Dark Calling"
    Fixed a bug where it would be able to summon Fusion monsters that do not require "Dark Fusion"

---
+++ script/official/c12071500.lua
@@ -0,0 +1,16 @@
+--ダーク・コーリング
+--Dark Calling
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Fusion.CreateSummonEff(c,function(c) return c.dark_calling end,Fusion.InHandMat(Card.IsAbleToRemove),s.fextra,Fusion.BanishMaterial,nil,nil,nil,SUMMON_TYPE_FUSION|0x10)
+	c:RegisterEffect(e1)
+	if not GhostBelleTable then GhostBelleTable={} end
+	table.insert(GhostBelleTable,e1)
+end
+s.listed_names={CARD_DARK_FUSION}
+function s.fextra(e,tp,mg)
+	if not Duel.IsPlayerAffectedByEffect(tp,69832741) then
+		return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToRemove),tp,LOCATION_GRAVE,0,nil)
+	end
+	return nil
+end
