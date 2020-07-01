efc00c7781886d3dbe636de8aa99f1e2d1df1b7d
larry126 <28985622+larry126@users.noreply.github.com>
2020-07-01 12:42:20 +0000

    Several fixes
    - Destiny HERO - Dusktopia (Anime) fixed, its given effect cannot be negated
    - Wroughtweiler (Anime) rescript to reflect its proxy text
    - Spell Calling (Anime) rescript to reflect its proxy text
    - Full-Force Strike updated either new client hint and hint for clarification

---
+++ script/unofficial/c511024001.lua
@@ -0,0 +1,50 @@
+--フレンドッグ (Anime)
+--Wroughtweiler (Anime)
+--Scripted by IanxWaifu, rescripted by Larry126
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_names={CARD_POLYMERIZATION}
+s.listed_series={0x3008}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
+end
+function s.filter1(c)
+	return c:IsSetCard(0x3008) and c:IsAbleToHand()
+end
+function s.filter2(c)
+	return c:IsCode(CARD_POLYMERIZATION) and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return true end
+	if Duel.IsExistingTarget(s.filter1,tp,LOCATION_GRAVE,0,1,nil)
+		and Duel.IsExistingTarget(s.filter2,tp,LOCATION_GRAVE,0,1,nil) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g1=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_GRAVE,0,1,1,nil)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g2=Duel.SelectTarget(tp,s.filter2,tp,LOCATION_GRAVE,0,1,1,nil)
+		g1:Merge(g2)
+		Duel.SetOperationInfo(0,CATEGORY_TOHAND,g1,2,0,0)
+	end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	if not g then return end
+	local sg=g:Filter(Card.IsRelateToEffect,nil,e)
+	if #sg==2 then
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
+end
---
+++ script/unofficial/c511024007.lua
@@ -0,0 +1,34 @@
+--コーリング・マジック (Anime)
+--Spell Calling (Anime)
+--Scripted by IanxWaifu
+local s,id=GetID()
+function s.initial_effect(c)
+	--Set
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCode(EVENT_DESTROYED)
+	e1:SetCondition(s.setcon)
+	e1:SetTarget(s.settg)
+	e1:SetOperation(s.setop)
+	c:RegisterEffect(e1)
+end
+function s.setcon(e,tp,eg,ep,ev,re,r,rp)
+	return re and re:IsActiveType(TYPE_SPELL+TYPE_TRAP) and e:GetHandler():IsPreviousControler(tp)
+end
+function s.filter(c)
+	return c:IsType(TYPE_SPELL) and c:IsSSetable()
+end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>=2
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,2,nil) end
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<2 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,2,2,nil)
+	if #g>0 then
+		Duel.SSet(tp,g)
+	end
+end
