ef64b4decf0d42636ea7ba4f0e9a54c5898db328
larry126 <28985622+larry126@users.noreply.github.com>
2020-07-03 01:09:45 +0000

    Update Baton of the Hero
    It now uses Patrician of Darkness' effect for accurate implementation

---
+++ script/unofficial/c511000849.lua
@@ -0,0 +1,25 @@
+--軍神の采配
+--Baton of the Hero
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_BATTLE_START)
+	e1:SetCondition(s.condition)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsTurnPlayer(1-tp) and Duel.IsAbleToEnterBP()
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_PATRICIAN_OF_DARKNESS)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(0,1)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
