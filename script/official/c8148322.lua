c333b9233f141139cc56ef3ddaa4b873fb8883c2
42Rundas <38040866+42Rundas@users.noreply.github.com>
2020-07-03 19:54:03 +0000

    Small "Fix" for Predaprime Fusion #301
    Removed an unused cost function to shorten the script

---
+++ script/official/c8148322.lua
@@ -0,0 +1,22 @@
+--プレデター・プライム・フュージョン
+--Predaprime Fusion
+--Scripted by edo9300
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Fusion.CreateSummonEff(c,aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_DARK),Fusion.OnFieldMat,s.fextra)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+	e1:SetCondition(s.condition)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x10f3}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0x10f3),tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+end
+function s.checkextra(tp,sg,fc)
+	return sg:IsExists(aux.AND(aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_DARK),aux.FilterBoolFunction(Card.IsControler,tp)),2,nil)
+end
+function s.fextra(e,tp,mg)
+	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsFaceup),tp,0,LOCATION_ONFIELD,nil),s.checkextra
+end
