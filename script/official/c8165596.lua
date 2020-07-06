ba1eba7c2e8993b4d4ba39565bcfb7e81f60a45e
edo9300 <edoardo762@gmail.com>
2020-07-06 11:21:49 +0000

    Fix Number 90: Galaxy-Eyes Photon Lord
    Was erroring out if no material was detached

---
+++ script/official/c8165596.lua
@@ -0,0 +1,97 @@
+--No.90 銀河眼の光子卿
+--Number 90: Galaxy-Eyes Photon Lord
+local s,id=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,nil,8,2)
+	c:EnableReviveLimit()
+	--indes
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.indcon)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--negate activate
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DISABLE+CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetCondition(s.negcon)
+	e2:SetCost(s.negcost)
+	e2:SetTarget(s.negtg)
+	e2:SetOperation(s.negop)
+	c:RegisterEffect(e2,false,REGISTER_FLAG_DETACH_XMAT)	
+	--search or attach
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetCountLimit(1,id+1)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetHintTiming(0,TIMING_END_PHASE)
+	e3:SetCondition(s.condition)
+	e3:SetTarget(s.target)
+	e3:SetOperation(s.operation)
+	c:RegisterEffect(e3)
+end
+s.listed_series={0x7b,0x55}
+s.xyz_number=90
+function s.indcon(e)
+	return e:GetHandler():GetOverlayGroup():IsExists(Card.IsSetCard,1,nil,0x55)
+end
+function s.negcon(e,tp,eg,ep,ev,re,r,rp,chk)
+	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and ep~=tp
+		and re:IsActiveType(TYPE_MONSTER) and Duel.IsChainDisablable(ev)
+end
+function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+	local ct=Duel.GetOperatedGroup():GetFirst()
+	e:SetLabelObject(ct)
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) and e:GetLabelObject() and e:GetLabelObject():IsSetCard(0x7b) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateEffect(ev) and e:GetLabelObject() and e:GetLabelObject():IsSetCard(0x7b) then
+		Duel.Destroy(eg,REASON_EFFECT)
+	end
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsStatus(STATUS_BATTLE_DESTROYED) then return false end
+	return Duel.GetTurnPlayer()~=tp
+end
+function s.filter(c)
+	return c:IsSetCard(0x55) or c:IsSetCard(0x7b)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsType(TYPE_XYZ) 
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local c=e:GetHandler()
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	local tc=g:GetFirst()
+	if (not c:IsRelateToEffect(e)) then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+	else aux.ToHandOrElse(tc,tp,function() return true end,
+						function() Duel.Overlay(e:GetHandler(),Group.FromCards(tc)) end,
+						aux.Stringid(id,3)
+						)
+	end
+end
+
