e7e7a3103cde36da9ff00210adf571fc2a73fad4
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-07-08 20:45:11 +0000

    "Miracle of Draconian Wrath" fix
    Was incorrectly using the targeting function for cards in the Deck.

---
+++ script/unofficial/c511000358.lua
@@ -0,0 +1,33 @@
+--奇跡の逆鱗
+--Miracle of Draconian Wrath
+local s,id=GetID()
+function s.initial_effect(c)	
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_DESTROYED)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CARD_TARGET)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(Card.IsRace,1,nil,RACE_DRAGON)
+end
+function s.filter(c)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsSSetable()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>1
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,2,nil) end
+	
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,2,2,nil)
+	if #g>0 then
+		Duel.SSet(tp,g)
+	end
+end
