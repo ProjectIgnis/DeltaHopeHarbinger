b27c8e77ea809256363ddd57d17b112157a81bc2
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-07-16 13:43:00 +0000

    Drone Pawn Fix
    Reworked completely the script of Drone Pawn since the recovering effect of the old script did not worked

---
+++ script/unofficial/c511009741.lua
@@ -0,0 +1,64 @@
+--ドローン・ポーン
+--Drone Pawn
+--fixed by CCM
+local s,id=GetID()
+function s.initial_effect(c)
+	--dam register
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EVENT_BATTLE_DAMAGE)
+	e1:SetCondition(s.atkcon)
+	e1:SetOperation(s.damop)
+	c:RegisterEffect(e1)
+	--LP
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_PHASE+PHASE_BATTLE)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCountLimit(1)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.reccon)
+	e2:SetTarget(s.rectg)
+	e2:SetOperation(s.recop)
+	c:RegisterEffect(e2)
+	--indes
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e3:SetCondition(s.condition)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return ep==tp and e:GetHandler()==Duel.GetAttackTarget()
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local dam=0
+	if c:GetFlagEffectLabel(id)~=nil then dam=c:GetFlagEffectLabel(id) end
+	c:ResetFlagEffect(id)
+	c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1,dam+ev)
+end
+function s.reccon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)<=1 and e:GetHandler():GetBattledGroupCount()>0 and  Duel.GetTurnPlayer()~=e:GetHandlerPlayer()
+end
+function s.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk ==0 then	return true end
+	local heal=e:GetHandler():GetFlagEffectLabel(id)
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(heal)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,heal)
+end
+function s.recop(e,tp,eg,ep,ev,re,r,rp)
+	local heal=e:GetHandler():GetFlagEffectLabel(id)
+	Duel.Recover(tp,heal,REASON_EFFECT)
+end
+function s.condition(e)
+	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)<=1
+end
