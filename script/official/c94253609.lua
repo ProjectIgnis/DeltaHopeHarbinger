d706e41d56ac802169700be983504b896d7a55b2
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-07-18 11:00:52 +0000

    "Elemental Absorber" fix
    Partial rescript. Banishing a monster from your hand is not a cost, it happens during resolution according to the OCG database.

---
+++ script/official/c94253609.lua
@@ -0,0 +1,42 @@
+--エレメンタル・アブソーバー
+--Elemental Absorber
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	--Opponent's monsters with the same Attribute cannot attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_ATTACK)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(0,LOCATION_MZONE)
+	e2:SetTarget(s.atktarget)
+	c:RegisterEffect(e2)
+	e1:SetLabelObject(e2)
+end
+function s.cfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsAbleToRemove()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_HAND)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp,chk)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND,0,1,1,nil)
+	if #g>0 then
+		Duel.Remove(g,POS_FACEUP,REASON_COST)
+		local att=g:GetFirst():GetAttribute()
+		e:GetLabelObject():SetLabel(att)
+		e:GetHandler():SetHint(CHINT_ATTRIBUTE,att)
+	end
+end
+function s.atktarget(e,c)
+	return c:IsAttribute(e:GetLabel())
+end
