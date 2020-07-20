92c9958b9d93b254ecc91ce53fdeb9ec1edeacd9
NaimSantos <naimsantos45@gmail.com>
2020-07-20 17:53:07 +0000

    Use TIMINGS_CHECK_MONSTER constant instead of its value
    Also includes fixes for effect enumeration in the scripts that had the constant changed

---
+++ script/unofficial/c511002534.lua
@@ -0,0 +1,22 @@
+--竜の束縛
+--Dragon's Bind (Anime)
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddPersistentProcedure(c,0,s.filter,nil,nil,nil,TIMINGS_CHECK_MONSTER)
+	--cannot spsummon
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetTargetRange(1,1)
+	e2:SetTarget(s.splimit)
+	c:RegisterEffect(e2)
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsRace(RACE_DRAGON)
+end
+function s.splimit(e,c,tp,sumtp,sumpos)
+	local tc=e:GetHandler():GetFirstCardTarget()
+	return tc and c:IsAttackBelow(tc:GetBaseAttack())
+end
