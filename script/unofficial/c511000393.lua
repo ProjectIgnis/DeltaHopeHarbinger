de1a9cd648d966afc74c9f80b7a61a775eed29f3
Larry126 <28985622+larry126@users.noreply.github.com>
2020-07-22 02:29:00 +0000

    Update unofficial scripts
    - Beast-Concealed Mantra cleaned up
    - Forbidden Beast Watsumu cleaned up

---
+++ script/unofficial/c511000393.lua
@@ -0,0 +1,37 @@
+--封印獣ワツム
+--Forbidden Beast Watsumu
+local s,id=GetID()
+function s.initial_effect(c)
+	--add to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_BATTLE_DESTROYED)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x51e}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsReason(REASON_BATTLE) and Duel.IsPlayerAffectedByEffect(tp,511000380)
+end
+function s.filter(c)
+	return c:IsSetCard(0x51e) and c:IsLevelBelow(3) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,2,2,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,#g,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.IsPlayerAffectedByEffect(tp,511000380) then return end
+	local g=Duel.GetTargetCards(e)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
