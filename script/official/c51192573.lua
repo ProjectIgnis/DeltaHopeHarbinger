2f15c4b19b79449edf14a1e124d615df6605e8c7
Naim <naimsantos45@gmail.com>
2020-07-26 17:28:17 +0000

    Fix "Cosmic Horror Gangi'el" extra procedure

---
+++ script/official/c51192573.lua
@@ -0,0 +1,58 @@
+--宇宙獣ガンギル
+--Cosmic Horror Gangi'el
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=aux.AddNormalSummonProcedure(c,true,true,1,1,SUMMON_TYPE_TRIBUTE,aux.Stringid(id,0),s.otfilter)
+	--counter
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_COUNTER)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetTarget(s.cttg)
+	e2:SetOperation(s.ctop)
+	c:RegisterEffect(e2)
+	--atk def
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_UPDATE_ATTACK)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e3:SetCondition(s.adcon)
+	e3:SetTarget(s.adtg)
+	e3:SetValue(s.adval)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e4)
+end
+s.listed_series={0xc}
+s.counter_place_list={0x100e}
+function s.otfilter(c,tp)
+	return c:GetOwner()==1-Ftp
+end
+function s.cttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end 
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,g,1,0x100e,1)
+end
+function s.ctop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		tc:AddCounter(0x100e,1)
+	end
+end
+function s.adcon(e)
+	return Duel.GetCurrentPhase()==PHASE_DAMAGE_CAL and Duel.GetAttackTarget()
+end
+function s.adtg(e,c)
+	local bc=c:GetBattleTarget()
+	return bc and c:GetCounter(0x100e)~=0 and bc:IsSetCard(0xc)
+end
+function s.adval(e,c)
+	return c:GetCounter(0x100e)*-300
+end
