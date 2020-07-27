4489fcbf4e4bf06fb39983da5abb5687ab5a957f
Larry126 <28985622+larry126@users.noreply.github.com>
2020-07-27 10:32:01 +0000

    Unofficial script fixes
    - Fix Gogogo Dexia (Manga) and Gogogo Aristeria (Manga)
    - Fix Soprano the Melodious Songstress (Anime)

---
+++ script/unofficial/c511005624.lua
@@ -0,0 +1,28 @@
+--ゴゴゴアリステラ (Manga)
+--Gogogo Aristeria (Manga)
+--Scripted by GameMaster (GM)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Pos change
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SET_POSITION)
+	e1:SetCondition(s.condition)
+	e1:SetValue(POS_FACEUP_DEFENSE)
+	c:RegisterEffect(e1)
+	--Cannot attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(0,LOCATION_MZONE)
+	e2:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+	e2:SetCondition(s.condition)
+	e2:SetValue(s.atlimit)
+	c:RegisterEffect(e2)
+end
+function s.condition(e)
+	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsCode,511005623),e:GetHandlerPlayer(),LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+end
+function s.atlimit(e,c)
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_EARTH)
+end
