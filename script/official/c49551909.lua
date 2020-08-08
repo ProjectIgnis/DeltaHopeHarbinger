e5845e7cd6ca9baf1ea5e55ce59d39fec6734920
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-08-08 12:30:27 +0000

    "Heroic Retribution Sword" fix
    Missing line in the card script was causing the "battle damage is shared" effect to not be registered to the card.

---
+++ script/official/c49551909.lua
@@ -0,0 +1,80 @@
+--ヒロイック・リベンジ・ソード
+--Heroic Retribution Sword
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(TIMING_BATTLE_PHASE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCost(aux.RemainFieldCost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x6f}
+function s.filter(c)
+	return c:IsFaceup() and c:IsSetCard(0x6f)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
+	if chk==0 then return e:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsLocation(LOCATION_SZONE) or not c:IsRelateToEffect(e) or c:IsStatus(STATUS_LEAVE_CONFIRMED) then return end
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		Duel.Equip(tp,c,tc)
+		--Destroy after damage calculation
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+		e1:SetCategory(CATEGORY_DESTROY)
+		e1:SetDescription(aux.Stringid(id,0))
+		e1:SetCode(EVENT_BATTLED)
+		e1:SetRange(LOCATION_SZONE)
+		e1:SetCondition(s.descon)
+		e1:SetTarget(s.destg)
+		e1:SetOperation(s.desop)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		c:RegisterEffect(e1)
+		--Battle damage is shared
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_EQUIP)
+		e2:SetCode(EFFECT_ALSO_BATTLE_DAMAGE)
+		c:RegisterEffect(e2)
+		--Equip limit
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_EQUIP_LIMIT)
+		e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e3:SetValue(s.eqlimit)
+		e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+		c:RegisterEffect(e3)
+	else
+		c:CancelToGrave(false)
+	end
+end
+function s.eqlimit(e,c)
+	return c:GetControler()==e:GetOwnerPlayer() and c:IsSetCard(0x6f)
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	local ec=e:GetHandler():GetEquipTarget()
+	return Duel.GetAttackTarget()==ec or (Duel.GetAttacker()==ec and Duel.GetAttackTarget())
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler():GetEquipTarget():GetBattleTarget(),1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local bc=e:GetHandler():GetEquipTarget():GetBattleTarget()
+	if bc and bc:IsRelateToBattle() then
+		Duel.Destroy(bc,REASON_EFFECT)
+	end
+end
