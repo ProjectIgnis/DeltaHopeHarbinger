4353c2920341df4142da3a5ba58b96b059a16681
NaimSantos <naimsantos45@gmail.com>
2020-08-14 11:56:20 +0000

    Rescript "Parasite Fusioner (anime)" to match Fusion Summon procedure

---
+++ script/unofficial/c511002961.lua
@@ -0,0 +1,64 @@
+--パラサイト・フュージョナー (anime)
+--Parasite Fusioner (anime)
+--original script by MLD
+local s,id=GetID()
+function s.initial_effect(c)
+	--Fusion Substitute
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_FUSION_SUBSTITUTE)
+	e1:SetCondition(s.subcon)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(id)
+	e2:SetRange(LOCATION_ONFIELD)
+	c:RegisterEffect(e2)
+	--Perform a Fusion Summon
+	local params = {nil,Fusion.OnFieldMat,nil,nil,Fusion.ForcedHandler,s.stage2}
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetCondition(s.spcon)
+	e3:SetTarget(Fusion.SummonEffTG(table.unpack(params)))
+	e3:SetOperation(Fusion.SummonEffOP(table.unpack(params)))
+	c:RegisterEffect(e3)
+	--Destruction replacement effect
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_EQUIP)
+	e4:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e4:SetCode(EFFECT_DESTROY_SUBSTITUTE)
+	e4:SetValue(s.repval)
+	c:RegisterEffect(e4)
+end
+function s.subcon(e)
+	return e:GetHandler():IsFaceup() and e:GetHandler():IsOnField()
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local ph=Duel.GetCurrentPhase()
+	return ph~=PHASE_DAMAGE and ph~=PHASE_DAMAGE_CAL and Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+end
+function s.stage2(e,tc,tp,sg,chk)
+	local c=e:GetHandler()
+	if chk==1 and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and c:IsLocation(LOCATION_GRAVE)then
+		if not Duel.Equip(tp,e:GetHandler(),tc,false) then return end
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_EQUIP_LIMIT)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(s.eqlimit)
+		e1:SetLabelObject(tc)
+		c:RegisterEffect(e1)
+	end
+end
+function s.eqlimit(e,c)
+	return e:GetLabelObject()==c
+end
+function s.repval(e,re,r,rp)
+	return (r&REASON_BATTLE+REASON_EFFECT)~=0
+end
