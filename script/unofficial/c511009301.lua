fba04d4e4aedf8a819a83c9708db5d02b40de119
Larry126 <28985622+larry126@users.noreply.github.com>
2020-08-24 05:25:01 +0000

    Unofficial script fixes
    - Card of the Dragon King inflicts damage equals to ATK on field
    - Gladiator Beast's Valor fix, it should now works properly
    - Morley's Shield continuous trap property fix

---
+++ script/unofficial/c511009301.lua
@@ -0,0 +1,28 @@
+--剣闘獣の雄心
+--Gladiator Beast's Valor
+local s,id=GetID()
+function s.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(s.actcon)
+	c:RegisterEffect(e1)
+	--only attack GB
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_MUST_ATTACK_MONSTER)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(0,LOCATION_MZONE)
+	e2:SetValue(s.attg)
+	c:RegisterEffect(e2)
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x19)
+end
+function s.actcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.attg(e,c)
+	return s.cfilter(c)
+end
