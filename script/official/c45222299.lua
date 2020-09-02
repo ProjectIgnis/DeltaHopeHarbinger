086ad4ed27b7078bddae488d3e7df7c89783dfff
Naim <naimsantos45@gmail.com>
2020-09-02 22:12:20 +0000

    Update "Evigishki Gustkraken"

---
+++ script/official/c45222299.lua
@@ -0,0 +1,36 @@
+--イビリチュア・ガストクラーケ
+--Evigishki Gustkraken
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Reveal and shuffle cards into the Deck
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_RITUAL)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,0,LOCATION_HAND)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_HAND)
+	if #g==0 then return end
+	local ct=1
+	if #g>1 then ct=Duel.AnnounceNumber(tp,1,2) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+	g=g:RandomSelect(tp,ct)
+	Duel.ConfirmCards(tp,g)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local sg=g:Select(tp,1,1,nil)
+	Duel.SendtoDeck(sg,nil,2,REASON_EFFECT)
+	Duel.ShuffleHand(1-tp)
+end
