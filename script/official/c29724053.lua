5a41aefffd4ffaf520cfa8d1128eeaffed065dc3
edo9300 <edoardo762@gmail.com>
2020-09-08 20:36:10 +0000

    Update cards still manually checking global_check
    use aux.GlobalCheck() instead

---
+++ script/official/c29724053.lua
@@ -0,0 +1,51 @@
+--サモン・ゲート
+local s,id=GetID()
+function s.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--summon count limit
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetTargetRange(1,1)
+	e2:SetTarget(s.sumlimit)
+	c:RegisterEffect(e2)
+	--
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCode(id)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetTargetRange(1,1)
+	c:RegisterEffect(e3)
+	aux.GlobalCheck(s,function()
+		s[0]=3
+		s[1]=3
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+		ge1:SetCode(EVENT_SPSUMMON_SUCCESS)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1)
+		aux.AddValuesReset(function()
+			s[0]=3
+			s[1]=3
+		end)
+	end)
+end
+function s.sumlimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return c:IsLocation(LOCATION_EXTRA) and s[sump]<=0
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	for tc in aux.Next(eg) do
+		if tc:IsPreviousLocation(LOCATION_EXTRA) then
+			local p=tc:GetSummonPlayer()
+			s[p]=s[p]-1
+		end
+	end
+end
