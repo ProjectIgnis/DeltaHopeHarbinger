f5061999cad2d2df08200f7a51bc386d2a40f8c5
NaimSantos <naimsantos45@gmail.com>
2020-09-14 20:25:35 +0000

    Added support for "Mirror Master of the Ice Barrier" discard replacement
    Added new functions: Auxiliary.IceBarrierDiscardFilter, Auxiliary.IceBarrierDiscardGroup, Auxiliary.IceBarrierDiscardCost
    
    Added new constant: CARD_MIRRORMASTER_ICEBARRIER
    Updated old "Ice Barrier monsters"

---
+++ script/official/c65749035.lua
@@ -0,0 +1,46 @@
+--氷結界の龍 グングニール
+--Gungnir, Dragon of the Ice Barrier
+local s,id=GetID()
+function s.initial_effect(c)
+	--synchro summon
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTunerEx(Card.IsAttribute,ATTRIBUTE_WATER),1,99)
+	c:EnableReviveLimit()
+	--destroy
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCountLimit(1)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.costfilter(c)
+	return c:IsAbleToGraveAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local rt=Duel.GetTargetCount(aux.TRUE,tp,0,LOCATION_ONFIELD,nil)
+	if rt>2 then rt=2 end	
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	if chk==0 then return aux.IceBarrierDiscardCost(s.costfilter,true,1,rt)(e,tp,eg,ep,ev,re,r,rp,0) end
+	local cg=aux.IceBarrierDiscardCost(s.costfilter,true,1,rt)(e,tp,eg,ep,ev,re,r,rp,1)
+	e:SetLabel(cg)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+	local ct=e:GetLabel()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local eg=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,ct,ct,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,ct,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local rg=tg:Filter(Card.IsRelateToEffect,nil,e)
+	if #rg>0 then 
+		Duel.Destroy(rg,REASON_EFFECT)
+	end
+end
---
+++ script/official/c73061465.lua
@@ -0,0 +1,38 @@
+--氷結界の封魔団
+--Spellbreaker of the Ice Barrier
+local s,id=GetID()
+function s.initial_effect(c)
+	--act limit
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x2f}
+function s.cfilter(c)
+	return c:IsSetCard(0x2f) and c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return aux.IceBarrierDiscardCost(s.cfilter,false)(e,tp,eg,ep,ev,re,r,rp,0) end
+	aux.IceBarrierDiscardCost(s.cfilter,false)(e,tp,eg,ep,ev,re,r,rp,1)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFacedown() or not c:IsRelateToEffect(e) then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(1,1)
+	e1:SetValue(s.tgval)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END,3)
+	c:RegisterEffect(e1)
+end
+function s.tgval(e,re,rp)
+	return re:IsActiveType(TYPE_SPELL) and re:IsHasType(EFFECT_TYPE_ACTIVATE)
+end
