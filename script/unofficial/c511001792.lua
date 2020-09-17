a6be28ac3654612b99f8a46fb48a07816d19eac4
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2020-09-17 19:25:47 +0000

    Fix for "Number Karma" (#414)
    Should only inflict damage to the player(s) that do not control a face-up "Number" monster.

---
+++ script/unofficial/c511001792.lua
@@ -0,0 +1,37 @@
+--ナンバーズ・カルマ
+--Number Karma
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_PHASE+PHASE_END)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(s.damtg)
+	e2:SetOperation(s.damop)
+	c:RegisterEffect(e2)
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x48)
+end
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,PLAYER_ALL,500)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local g1=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_MZONE,0,nil)
+	local g2=Duel.GetMatchingGroup(s.cfilter,1-tp,LOCATION_MZONE,0,nil)
+	if #g1==0 then
+		Duel.Damage(tp,500,REASON_EFFECT,true)
+	end
+	if #g2==0 then
+		Duel.Damage(1-tp,500,REASON_EFFECT,true)
+	end
+	Duel.RDComplete()
+end
