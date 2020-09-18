ee7975d5544a557d9fe7a498418158545f5e71c7
Naim <naimsantos45@gmail.com>
2020-09-18 10:27:28 +0000

    Fix Toon cards (#420)
    Toon Rollback cannot be used in a monster that cannot attack
    
    Update Toon monsters to match ruling with Skill Drain and Toon Rollback

---
+++ script/official/c15270885.lua
@@ -0,0 +1,77 @@
+--トゥーン・ゴブリン突撃部隊
+--Toon Goblin Attack Force
+local s,id=GetID()
+function s.initial_effect(c)
+	--Cannot attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetOperation(s.atklimit)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	--Destroy
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCode(EVENT_LEAVE_FIELD)
+	e4:SetCondition(s.sdescon)
+	e4:SetOperation(s.sdesop)
+	c:RegisterEffect(e4)
+	--Direct attack
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetCode(EFFECT_DIRECT_ATTACK)
+	e5:SetCondition(s.dircon)
+	c:RegisterEffect(e5)
+	--Change to defense
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e6:SetCode(EVENT_PHASE+PHASE_BATTLE)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCountLimit(1)
+	e6:SetCondition(s.poscon)
+	e6:SetOperation(s.posop)
+	c:RegisterEffect(e6)
+end
+s.listed_names={15259703}
+function s.atklimit(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_ATTACK)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	e:GetHandler():RegisterEffect(e1)
+end
+function s.sfilter(c)
+	return c:IsReason(REASON_DESTROY) and c:IsPreviousPosition(POS_FACEUP) and c:GetPreviousCodeOnField()==15259703 and c:IsPreviousLocation(LOCATION_ONFIELD)
+end
+function s.sdescon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.sfilter,1,nil)
+end
+function s.sdesop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+end
+function s.dircon(e)
+	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsCode,15259703),e:GetHandlerPlayer(),LOCATION_ONFIELD,0,1,nil)
+		and not Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsType,TYPE_TOON),e:GetHandlerPlayer(),0,LOCATION_MZONE,1,nil)
+end
+function s.poscon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetAttackedCount()>0
+end
+function s.posop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsAttackPos() then
+		Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
+	end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_COPY_INHERIT)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,3)
+	c:RegisterEffect(e1)
+end
