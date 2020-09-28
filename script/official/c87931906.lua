1d0f214a041961d82f85d8554324f1bafca6d5c5
senpaizuri3 <senpaizuri3@gmail.com>
2020-09-28 03:37:24 +0000

    Small fix "Lunalight Fusion"
    Now the effect of "Ghost Belle & Haunted Mansion" isn't erroneously able to activate in response, but now "Ash Blossom & Joyous Spring" can.

---
+++ script/official/c87931906.lua
@@ -0,0 +1,26 @@
+--月光融合
+--Lunalight Fusion
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Fusion.CreateSummonEff(c,aux.FilterBoolFunction(Card.IsSetCard,0xdf),nil,s.fextra)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	c:RegisterEffect(e1)
+	AshBlossomTable=AshBlossomTable or {}
+	table.insert(AshBlossomTable,e1)
+end
+s.listed_series={0xdf}
+function s.fcheck(tp,sg,fc)
+	return sg:FilterCount(Card.IsLocation,nil,LOCATION_EXTRA+LOCATION_DECK)<=1
+end
+function s.fextra(e,tp,mg)
+	if Duel.IsExistingMatchingCard(Card.IsSummonLocation,tp,0,LOCATION_MZONE,1,nil,LOCATION_EXTRA) then
+		local eg=Duel.GetMatchingGroup(s.exfilter,tp,LOCATION_EXTRA+LOCATION_DECK,0,nil)
+		if eg and #eg>0 then
+			return eg,s.fcheck
+		end
+	end
+	return nil
+end
+function s.exfilter(c)
+	return c:IsMonster() and c:IsSetCard(0xdf) and c:IsAbleToGrave()
+end
