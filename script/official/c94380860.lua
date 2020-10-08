5f1d9a1a8fa307a8c5e3910ab2135bfd5d493d99
senpaizuri3 <senpaizuri3@gmail.com>
2020-10-08 21:46:47 +0000

    Fix "Number 103: Ragnazero"
    The effect will now ignore indirect changes to ATK by paying the cost, when choosing a target.

---
+++ script/official/c94380860.lua
@@ -0,0 +1,54 @@
+--No.103 神葬零嬢ラグナ・ゼロ
+local s,id=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,nil,4,2)
+	c:EnableReviveLimit()
+	--
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
+end
+s.xyz_number=103
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function s.filter(c,e)
+	return c:IsPosition(POS_FACEUP_ATTACK) and c:GetAttack()~=c:GetBaseAttack()
+		and c:IsCanBeEffectTarget(e)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc,e) end
+	if chk==0 then
+		--retain applicable targets in case cost makes an indirect change to ATK
+		local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,e)
+		if #g==0 or not Duel.IsPlayerCanDraw(tp,1) then return end
+		g:KeepAlive()
+		e:SetLabelObject(g)
+		return true
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=e:GetLabelObject()
+	local sg=g:Select(tp,1,1,nil)
+	Duel.SetTargetCard(sg)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,sg,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if not tc then return end
+	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 then
+		Duel.Draw(tp,1,REASON_EFFECT)
+	end
+end
