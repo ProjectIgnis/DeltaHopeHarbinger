fd1aba9ea6e5402a875af811f8dad7b831a23a0d
Larry126 <28985622+larry126@users.noreply.github.com>
2020-10-19 07:40:11 +0000

    Update unofficial script
    Minor change to trigger delta flow

---
+++ script/unofficial/c511003050.lua
@@ -0,0 +1,20 @@
+--ＣＣ 隻眼のパスト・アイ
+--C/C Critical Eye
+local s,id=GetID()
+function s.initial_effect(c)
+	--Fusion, synchro, and Xyz material limitations
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_FUSION_MAT_RESTRICTION)
+	e1:SetValue(s.filter)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_SYNCHRO_MAT_RESTRICTION)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetCode(EFFECT_XYZ_MAT_RESTRICTION)
+	c:RegisterEffect(e3)
+end
+function s.filter(e,c)
+	return not c:IsControler(e:GetHandlerPlayer())
+end
---
+++ script/unofficial/c511015113.lua
@@ -0,0 +1,61 @@
+--写真融合
+--Montage Fusion
+--Re-scripted by Larry126
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--Fusion Summon
+	local params={matfilter=Fusion.OnFieldMat,extrafil=s.fextra,extraop=s.extraop,stage2=s.stage2}
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(Fusion.SummonEffTG(params))
+	e2:SetOperation(Fusion.SummonEffOP(params))
+	c:RegisterEffect(e2)
+end
+function s.fcheck(tp,sg,fc)
+	return sg:FilterCount(Card.IsControler,nil,tp)>=1 and sg:FilterCount(Card.IsControler,nil,1-tp)>=1
+end
+function s.fextra(e,tp,mg)
+	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsFaceup),tp,0,LOCATION_ONFIELD,nil),s.fcheck
+end
+function s.extraop(e,tc,tp,sg)
+	sg:Sub(sg:Filter(Card.IsControler,nil,1-tp))
+	tc:SetMaterial(sg)
+end
+function s.stage2(e,tc,tp,sg,chk)
+	if chk==3 then
+		local c=e:GetHandler()
+		local fid=c:GetFieldID()
+		tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1,fid)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
+		e1:SetCountLimit(1)
+		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e1:SetLabel(fid)
+		e1:SetLabelObject(tc)
+		e1:SetCondition(s.descon)
+		e1:SetOperation(s.desop)
+		Duel.RegisterEffect(e1,tp)
+	end
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	if tc:GetFlagEffectLabel(id)==e:GetLabel() then
+		return true
+	else
+		e:Reset()
+		return false
+	end
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Destroy(e:GetLabelObject(),REASON_EFFECT)
+end
