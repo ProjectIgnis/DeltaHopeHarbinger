4b21c5eb73fb2387955d2a6db8556c20e3280ea4
Larry126 <28985622+larry126@users.noreply.github.com>
2020-10-25 14:07:09 +0000

    Performapal Handstandacoon (Anime) script clean-up
    Fix various script inconsistencies and errors.

---
+++ script/unofficial/c511009162.lua
@@ -0,0 +1,53 @@
+--ＥＭリターンタンタン (Anime)
+--Performapal Handstandacoon (Anime)
+local s,id,alias=GetID()
+function s.initial_effect(c)
+	alias=c:Alias()
+	--Pendulum Summon
+	Pendulum.AddProcedure(c)
+	--Pendulum Effect
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(alias,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.target1)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	 --Return
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(alias,0))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_BATTLE_DESTROYED)
+	e2:SetCondition(s.condition)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(tp) and chkc:IsAbleToHand() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToHand,tp,LOCATION_ONFIELD,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,LOCATION_ONFIELD,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return not e:GetHandler():IsLocation(LOCATION_DECK)
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsAbleToHand() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToHand,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
