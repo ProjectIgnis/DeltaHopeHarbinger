ebbb097efe135417ce65282e2c50afe6b055074d
Larry126 <28985622+larry126@users.noreply.github.com>
2020-11-10 13:58:33 +0000

    Add new unofficial cards script
    - Starliege Lord Galaxion (Anime)
    - Paladin of Photon Dragon (Anime)
    - Xyz Block (Anime)

---
+++ script/unofficial/c511310017.lua
@@ -0,0 +1,39 @@
+--異次元トンネル－ミラーゲート－ (Anime)
+--Mirror Gate (Anime)
+--Scripted by AlphaKretin
+--Fixed by MLD
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_CONTROL)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local at=Duel.GetAttackTarget()
+	return a:IsControler(1-tp) and at and at:IsControler(tp)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local a=Duel.GetAttacker()
+	local at=Duel.GetAttackTarget()
+	if chk==0 then return a:IsOnField() and a:IsAbleToChangeControler()
+		and at:IsOnField() and at:IsAbleToChangeControler() end
+	local g=Group.FromCards(a,at)
+	Duel.SetTargetCard(g)
+	Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,2,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local at=Duel.GetAttackTarget()
+	if a:IsRelateToEffect(e) and a:CanAttack() and at:IsRelateToEffect(e) then
+		if Duel.SwapControl(a,at) then
+			Duel.CalculateDamage(a,at)
+		end
+	end
+end
