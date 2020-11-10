94e07565de2bf282e39392e89c4ef769d24639b4
Dylam De La Torre <dyxel04@gmail.com>
2020-11-10 20:11:02 +0000

    update "Clay Charge"
    * Was targeting when it shouldn't

---
+++ script/official/c22479888.lua
@@ -0,0 +1,37 @@
+--クレイ・チャージ
+--Clay Charge
+
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_BE_BATTLE_TARGET)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_names={84327329}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local at=Duel.GetAttackTarget()
+	return at:IsControler(tp) and at:IsCode(84327329)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local a=Duel.GetAttacker()
+	local at=Duel.GetAttackTarget()
+	if chkc then return false end
+	if chk==0 then return a:IsOnField() and at:IsOnField() end
+	if at:IsFacedown() then Duel.ConfirmCards(1-tp,at) end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,Group.FromCards(a,at),2,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,800)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local at=Duel.GetAttackTarget()
+	if a and a:IsRelateToBattle() and at and at:IsRelateToBattle() then
+		Duel.Destroy(Group.FromCards(a,at),REASON_EFFECT)
+		Duel.Damage(1-tp,800,REASON_EFFECT)
+	end
+end
