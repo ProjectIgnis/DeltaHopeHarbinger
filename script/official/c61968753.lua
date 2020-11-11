7477a17270c9a136874d06b05ccc362af4443e3f
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-11-11 20:21:50 +0000

    Updated "Bubble Shuffle"
    Was failing to continue the effect beyond the battle position change if the player's monster zones were full.

---
+++ script/official/c61968753.lua
@@ -0,0 +1,62 @@
+--バブル・シャッフル
+--Bubble Shuffle
+
+local s,id=GetID()
+function s.initial_effect(c)
+	--Tribute "Elemental HERO Bubbleman", then special summon 1 "Elemental HERO" monster from hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_POSITION+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.postg)
+	e1:SetOperation(s.posop)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x3008}
+s.listed_names={79979666}
+
+function s.filter1(c,ft)
+	return c:IsPosition(POS_FACEUP_ATTACK) and c:IsCanChangePosition() and c:IsCode(79979666) 
+		and (ft>0 or c:GetSequence()<5)
+end
+function s.filter2(c)
+	return c:IsPosition(POS_FACEUP_ATTACK) and c:IsCanChangePosition()
+end
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x3008) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.postg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if chk==0 then return ft>-1 and Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE,0,1,nil,ft)
+		and Duel.IsExistingTarget(s.filter2,tp,0,LOCATION_MZONE,1,nil)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+	local g1=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE,0,1,1,nil,ft)
+	e:SetLabelObject(g1:GetFirst())
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+	local g2=Duel.SelectTarget(tp,s.filter2,tp,0,LOCATION_MZONE,1,1,nil)
+	g1:Merge(g2)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,g1,2,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.pfilter(c,e)
+	return c:IsPosition(POS_FACEUP_ATTACK) and c:IsRelateToEffect(e)
+end
+function s.posop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(s.pfilter,nil,e)
+	if #g==2 then
+		Duel.ChangePosition(g,POS_FACEUP_DEFENSE)
+		local tc=e:GetLabelObject()
+		if not tc:IsImmuneToEffect(e) and tc:IsReleasable() and (Duel.GetLocationCount(tp,LOCATION_MZONE)>0 or tc:GetSequence()<5) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+			Duel.BreakEffect()
+			Duel.Release(tc,REASON_EFFECT)
+			Duel.BreakEffect()
+			Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
