2852f856989fc073af9f3629fdef82d104296928
Larry126 <28985622+larry126@users.noreply.github.com>
2020-11-13 15:07:51 +0000

    Minor edits to trigger delta flow

---
+++ script/official/c78063197.lua
@@ -0,0 +1,62 @@
+--サクリファイス・フュージョン
+--Relinquished Fusion
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Fusion.CreateSummonEff(c,aux.FilterBoolFunction(Card.IsSetCard,0x1110),Card.IsAbleToRemove,s.fextra,Fusion.BanishMaterial)
+	e1:SetCountLimit(1,id)
+	c:RegisterEffect(e1)
+	if not GhostBelleTable then GhostBelleTable={} end
+	table.insert(GhostBelleTable,e1)
+	--equip
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCountLimit(1,id+1)
+	e2:SetCategory(CATEGORY_EQUIP)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.eqtg)
+	e2:SetOperation(s.eqop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x1110}
+s.listed_names={64631466}
+function s.fextra(e,tp,mg)
+	if not Duel.IsPlayerAffectedByEffect(tp,69832741) then
+		return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToRemove),tp,LOCATION_GRAVE,0,nil)
+	end
+	return nil
+end
+function s.filter(c,tp)
+	return c:IsFaceup() and c:IsType(TYPE_EFFECT) and c:IsAbleToChangeControler() 
+		and Duel.IsExistingMatchingCard(s.eqfilter,tp,LOCATION_MZONE,0,1,nil,c,tp)
+end
+function s.eqfilter(c,ec,tp)
+	local eff={c:GetCardEffect(89785779)}
+	if c:IsFacedown() or ((not c:IsSetCard(0x1110) or not c:IsType(TYPE_FUSION)) and not c:IsCode(64631466)) then return false end
+	for _,te in ipairs(eff) do
+		if te:GetValue()(ec,c,tp) then return true end
+	end
+	return false
+end
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_MZONE,1,nil,tp) and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil,tp)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
+end
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+	local tc1=Duel.GetFirstTarget()
+	if not tc1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectMatchingCard(tp,s.eqfilter,tp,LOCATION_MZONE,0,1,1,nil,tc1,tp)
+	local tc2=g:GetFirst()
+	if not tc2 then return end
+	local te=tc2:GetCardEffect(89785779)
+	if tc1:IsFaceup() and tc1:IsRelateToEffect(e) and tc1:IsControler(1-tp) then
+		te:GetOperation()(tc2,te:GetLabelObject(),tp,tc1)
+	end
+end
