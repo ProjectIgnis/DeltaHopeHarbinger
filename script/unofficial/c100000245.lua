7419bac2b9e02574d544c34f6891134f5a5531a0
Larry126 <28985622+larry126@users.noreply.github.com>
2020-11-27 15:50:39 +0000

    Updated various scripts
    - "T.G. Drill Fish" cleanup
    - "Amazoness Village" cleanup
    - "T.G. Metal Skeleton (VG)" fixed, the condition should be sent to the Graveyard
    - "T.G. Drill Fish (VG)" fixed, last effect should target, and optional

---
+++ script/unofficial/c100000245.lua
@@ -0,0 +1,41 @@
+--ＴＧ ドリル・フィッシュ (VG)
+--T.G. Drill Fish (VG)
+local s,id=GetID()
+function s.initial_effect(c)
+	--direct attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_DIRECT_ATTACK)
+	e1:SetCondition(s.dircon)
+	c:RegisterEffect(e1)
+	--destroy
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_BATTLE_DAMAGE)
+	e2:SetCondition(s.descon)
+	e2:SetTarget(s.destg)
+	e2:SetOperation(s.desop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x27}
+function s.dircon(e)
+	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0x27),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,e:GetHandler())
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp,chk)
+	return ep~=tp and Duel.GetAttackTarget()==nil
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(nil,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,nil,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end
