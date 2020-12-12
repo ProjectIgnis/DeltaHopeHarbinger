281121ec219678978541b235c9d7420f8ea43e15
Dylam De La Torre <dyxel04@gmail.com>
2020-12-12 02:12:07 +0000

    fix Amazement Family Face
    * was sending monster to GY after resolution

--- script/pre-release/c101104070.lua
+++ script/pre-release/c101104070.lua
@@ -1,10 +1,20 @@
 --アメイズメント・ファミリーフェイス
 --Amazement Family Face
 --Scripted by Eerie Code
+
 local s,id=GetID()
 function s.initial_effect(c)
-	local e0=aux.AddEquipProcedure(c,1,s.eqfilter,s.eqlimit,nil,s.target)
+	--Equip this card to 1 monster
+	local e0=Effect.CreateEffect(c)
+	e0:SetCategory(CATEGORY_CONTROL+CATEGORY_EQUIP)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	e0:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e0:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e0:SetCost(aux.RemainFieldCost)
+	e0:SetTarget(s.target)
+	e0:SetOperation(s.activate)
+	c:RegisterEffect(e0)
 	--atk up
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_EQUIP)
@@ -25,22 +35,49 @@ function s.initial_effect(c)
 	e3:SetValue(0x25d)
 	e3:SetCondition(s.condition)
 	c:RegisterEffect(e3)
+	--control
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_EQUIP)
+	e4:SetCode(EFFECT_SET_CONTROL)
+	e4:SetValue(function(e)return e:GetHandlerPlayer()end)
+	c:RegisterEffect(e4)
 end
 s.listed_series={0x25d,0x25e}
-function s.eqfilter(c,e,tp)
-	return aux.CheckStealEquip(c) and c:GetEquipGroup():IsExists(s.eqcfilter,1,nil,tp)
-end
 function s.eqcfilter(c,tp)
 	return c:IsSetCard(0x25e) and c:IsType(TYPE_TRAP) and c:IsControler(tp)
+end
+function s.eqfilter(c,e,tp)
+	return aux.CheckStealEquip(c,e,tp) and c:GetEquipGroup():IsExists(s.eqcfilter,1,nil,tp)
 end
 function s.eqlimit(e,c)
 	return e:GetHandlerPlayer()~=c:GetControler() or e:GetHandler():GetEquipTarget()==c
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,tc)
-	e:SetCategory(CATEGORY_CONTROL+CATEGORY_EQUIP)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.eqfilter(chkc,e,tp) end
+	if chk==0 then return e:IsHasType(EFFECT_TYPE_ACTIVATE)
+		and Duel.IsExistingTarget(s.eqfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	Duel.SelectTarget(tp,s.eqfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_CONTROL,tc,1,0,0)
 end
-function s.condition(e)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:GetControler()==e:GetHandlerPlayer()
+	if not c:IsLocation(LOCATION_SZONE) or not c:IsRelateToEffect(e) or c:IsStatus(STATUS_LEAVE_CONFIRMED) then return end
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		Duel.Equip(tp,c,tc)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_EQUIP_LIMIT)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetValue(s.eqlimit)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		c:RegisterEffect(e1)
+	else
+		c:CancelToGrave(false)
+	end
+end
+function s.condition(e)
+	return e:GetHandler():GetControler()==e:GetHandlerPlayer()
 end
