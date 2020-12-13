81e301ad08afca1dddd42d17c0c0bfad9e590fd5
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2020-12-13 13:40:55 +0000

    Fix for "Chronic Deja Vu" (#497)
    * Fix for "Chronic Deja Vu"
    
    Token procedure was not treating the tokens as Link monsters if a Link monster was SS'd
    
    Co-authored-by: Larry126 <28985622+larry126@users.noreply.github.com>

---
+++ script/unofficial/c511000730.lua
@@ -0,0 +1,43 @@
+--クロニック・デジャヴュ
+--Chronic Déjà Vu
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
+end
+function s.filter(c,tp)
+	return c:IsFaceup() and c:GetSummonPlayer()~=tp
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return eg:IsExists(s.filter,1,nil,tp) end
+	Duel.SetTargetCard(eg)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,#eg,0,0)
+end
+function s.filter2(c,e,tp)
+	return c:IsFaceup() and not c:IsSummonPlayer(tp) and c:IsRelateToEffect(e) 
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=eg:Filter(s.filter2,nil,e,tp)
+	for tc in aux.Next(g) do
+		local code=tc:GetCode()
+		local token=Duel.CreateToken(tp,code)
+		Duel.SpecialSummonStep(token,0,tp,tp,true,false,tc:GetPosition())
+	end
+	Duel.SpecialSummonComplete()
+end
