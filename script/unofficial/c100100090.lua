1ac333209373133d25127f9d860ebcee6a6d26e1
Larry126 <28985622+larry126@users.noreply.github.com>
2020-12-14 11:07:58 +0000

    Fix "Acceleration Limiter" in SW2

---
+++ script/unofficial/c100100090.lua
@@ -0,0 +1,17 @@
+--ゲート・ブロッカー
+local s,id=GetID()
+function s.initial_effect(c)
+	--
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(id)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.con)
+	e1:SetTargetRange(0,1)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+end
+function s.con(e)
+	return Duel.IsEnvironment(511600371,1-e:GetHandlerPlayer())
+end
---
+++ script/unofficial/c100100091.lua
@@ -0,0 +1,48 @@
+--ゲート・ブロッカー２
+--Gate Blocker 2
+local s,id=GetID()
+function s.initial_effect(c)
+	--spsummon from hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetCondition(s.hspcon)
+	e1:SetTarget(s.hsptg)
+	e1:SetOperation(s.hspop)
+	c:RegisterEffect(e1)
+	--
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(100100090)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.con)
+	e2:SetTargetRange(0,1)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+end
+s.listed_names={8102334}
+function s.hspcon(e,c)
+	if c==nil then return true end
+	return Duel.CheckReleaseGroup(c:GetControler(),Card.IsCode,1,false,1,true,c,c:GetControler(),nil,false,nil,8102334)
+end
+function s.hsptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectReleaseGroup(tp,Card.IsCode,1,1,false,true,true,c,nil,nil,false,nil,8102334)
+	if g then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+	return true
+	end
+	return false
+end
+function s.hspop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.Release(g,REASON_COST)
+	g:DeleteGroup()
+end
+function s.con(e)
+	return Duel.IsEnvironment(511600371,1-e:GetHandlerPlayer())
+end
