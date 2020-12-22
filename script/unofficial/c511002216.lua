3b177937f46b338c8bd9f40949c9d00b5b38f02a
senpaizuri3 <senpaizuri3@gmail.com>
2020-12-22 02:13:42 +0000

    Update "Black Rose Seed" script
    updated script to reflect cdb update

---
+++ script/unofficial/c511002216.lua
@@ -0,0 +1,39 @@
+--黑薔薇の種
+--Black Rose Seed
+local s,id=GetID()
+function s.initial_effect(c)
+	--atkup
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x123}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetAttacker()
+	if tc:IsControler(1-tp) then tc=Duel.GetAttackTarget() end
+	e:SetLabelObject(tc)
+	return tc and tc:IsFaceup() and tc:IsSetCard(0x123) and tc:GetBattleTarget()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=e:GetLabelObject()
+	if chk==0 then return true end
+	Duel.SetTargetCard(tc)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=Duel.GetFirstTarget()
+	if not tc:IsFaceup() or not tc:IsRelateToBattle() then return end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE_CAL)
+	e1:SetValue(800)
+	tc:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	tc:RegisterEffect(e2)
+end
